b0468f186c40a0c7499b87e2883549ceb58b30fa
Karl Trygve Kalleberg <karltk@gmail.com>
2006-04-02 14:28:54 +0000

    Added sglr-like parameters.
    svn path=/spoofax/trunk/spoofax/org.spoofax.jsglr/; revision=14887

--- org.spoofax.jsglr/src/org/spoofax/jsglr/Main.java
+++ org.spoofax.jsglr/src/org/spoofax/jsglr/Main.java
@@ -9,7 +9,9 @@ package org.spoofax.jsglr;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 import aterm.ATerm;
 
@@ -17,16 +19,70 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException, IOException, InvalidParseTableException {
         
-        SGLR sglr= new SGLR();
-        
         if(args.length < 2) {
-            System.out.println("Usage: org.spoofax.jsglr.Main <parsetable.tbl> <inputfile>");
-            System.exit(-1);
+            usage();
         }
-        sglr.loadParseTable(new FileInputStream(args[0]));
-        FileInputStream fis= new FileInputStream(args[1]);
+
+        String parseTable = null;
+        String input = null;
+        String output = null;
+        boolean debugging = false;
+        boolean logging = false;
+        boolean detectCycles = true;
+        boolean filter = true;
         
+        int skip = 0;
+        for(int i=0;i<args.length;i++) {
+            if(skip > 0)
+                skip--;
+            if(args[i].equals("-p")) {
+                parseTable = args[i + 1];
+                skip = 1;
+            } else if(args[i].equals("-i")) {
+                input = args[i + 1];
+                skip = 1;
+            } else if(args[i].equals("-o")) {
+                output = args[i + 1];
+                skip = 1;
+            } else if(args[i].equals("-d")) {
+                debugging = true;
+            } else if(args[i].equals("-v")) {
+                logging = true;
+            } else if(args[i].equals("-f")) {
+                filter = false;
+            } else if(args[i].equals("-c")) {
+                detectCycles = false;
+            }
+        }
+
+        if(input == null || parseTable == null)
+            usage();
+        
+
+        SGLR sglr = new SGLR();
+
+        sglr.setDebug(debugging);
+        sglr.setLogging(logging);
+        sglr.setCycleDetect(detectCycles);
+        sglr.setFilter(filter);
+        
+        sglr.loadParseTable(new FileInputStream(parseTable));
+        FileInputStream fis= new FileInputStream(input);
+        OutputStream ous = null;
+        if(output != null)
+            ous = new FileOutputStream(output);
+        else 
+            ous = System.out;
+            
         ATerm t = sglr.parse(fis);
-        System.out.println(t);
+        if(t != null)
+            ous.write(t.toString().getBytes());
+        else
+            System.err.println("Parse error");
+    }
+
+    private static void usage() {
+        System.out.println("Usage: org.spoofax.jsglr.Main -p <parsetable.tbl> -i <inputfile>");
+        System.exit(-1);
     }
 }

cea277ef72f5b555aeb09f8ce249b1a1455814b3
Karl Trygve Kalleberg <karltk@gmail.com>
2006-04-17 17:32:51 +0000

    Missing test data and test suite.
    svn path=/spoofax/trunk/spoofax/org.spoofax.jsglr/; revision=14962

---
+++ org.spoofax.jsglr/src/org/spoofax/jsglr/tests/AllTests.java
@@ -0,0 +1,27 @@
+/*
+ * Created on 17.apr.2006
+ *
+ * Copyright (c) 2005, Karl Trygve Kalleberg <karltk@ii.uib.no>
+ * 
+ * Licensed under the GNU General Public License, v2
+ */
+package org.spoofax.jsglr.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite("Test for org.spoofax.jsglr.tests");
+        //$JUnit-BEGIN$
+        suite.addTestSuite(TestG0.class);
+        suite.addTestSuite(TestG1.class);
+        suite.addTestSuite(TestStratego.class);
+        suite.addTestSuite(TestBooleans.class);
+        suite.addTestSuite(TestG2.class);
+        //$JUnit-END$
+        return suite;
+    }
+
+}

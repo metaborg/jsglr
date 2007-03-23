182b8106fb5db1b06287c14ec1f2264db4382ffe
Karl Trygve Kalleberg <karltk@gmail.com>
2007-03-23 21:03:08 +0000

    Load Stratego parse table on demand.
    svn path=/spoofax/trunk/spoofax/org.spoofax.interpreter.library.jsglr/; revision=16898

--- org.spoofax.interpreter.library.jsglr/src/org/spoofax/interpreter/library/jsglr/JSGLRLibrary.java
+++ org.spoofax.interpreter.library.jsglr/src/org/spoofax/interpreter/library/jsglr/JSGLRLibrary.java
@@ -1,13 +1,14 @@
 package org.spoofax.interpreter.library.jsglr;
 
-import java.io.IOException;
-
+import org.spoofax.interpreter.adapter.aterm.WrappedATermFactory;
 import org.spoofax.interpreter.library.AbstractStrategoOperatorRegistry;
-import org.spoofax.interpreter.terms.ITermFactory;
-import org.spoofax.jsglr.InvalidParseTableException;
 
 public class JSGLRLibrary extends AbstractStrategoOperatorRegistry {
-    public JSGLRLibrary(ITermFactory factory) throws IOException, InvalidParseTableException {
+    
+	public static final String REGISTRY_NAME = "JSGLR";
+
+	public JSGLRLibrary(WrappedATermFactory factory) {
         add(new JSGLR_parse_stratego(factory));
     }
+
 }
--- org.spoofax.interpreter.library.jsglr/src/org/spoofax/interpreter/library/jsglr/JSGLR_parse_stratego.java
+++ org.spoofax.interpreter.library.jsglr/src/org/spoofax/interpreter/library/jsglr/JSGLR_parse_stratego.java
@@ -12,7 +12,6 @@ import org.spoofax.interpreter.adapter.aterm.WrappedATermFactory;
 import org.spoofax.interpreter.library.AbstractPrimitive;
 import org.spoofax.interpreter.terms.IStrategoString;
 import org.spoofax.interpreter.terms.IStrategoTerm;
-import org.spoofax.interpreter.terms.ITermFactory;
 import org.spoofax.jsglr.InvalidParseTableException;
 import org.spoofax.jsglr.ParseTable;
 import org.spoofax.jsglr.ParseTableManager;
@@ -24,27 +23,25 @@ import aterm.pure.PureFactory;
 
 public class JSGLR_parse_stratego extends AbstractPrimitive {
 	private SGLR StrategoSGLR;
+	private WrappedATermFactory factory;
 	
-	JSGLR_parse_stratego(ITermFactory factory) throws IOException, InvalidParseTableException
-	{
+	JSGLR_parse_stratego(WrappedATermFactory factory) {
 		super("JSGLR_parse_stratego", 0, 1);
-        // FIXME this must be cleaned
-		PureFactory pf = ((WrappedATermFactory) factory).getFactory();
-		ParseTableManager ptm = new ParseTableManager(pf);
-	
-		ParseTable pt = ptm.loadFromFile("data/Stratego.tbl");
-		StrategoSGLR = new SGLR(pf, pt);
+		this.factory = factory;
 	}
 	
 	@Override
 	public boolean call(IContext env, IConstruct[] svars,
 			IStrategoTerm[] tvars) throws InterpreterException {
-		System.err.println("Reading: ");
+		
 		if (!Tools.isTermString(tvars[0]))
 			return false;
 		String path = ((IStrategoString)tvars[0]).getValue();
-		System.err.println(path);
 		ATerm parsed = null;
+		if(StrategoSGLR == null)
+			initialize();
+		if(StrategoSGLR == null)
+			return false;
 		try {
 			parsed = StrategoSGLR.parse(new FileInputStream(path));
 			System.err.println("Yes!");
@@ -65,6 +62,24 @@ public class JSGLR_parse_stratego extends AbstractPrimitive {
         WrappedATermFactory fac = (WrappedATermFactory) env.getFactory();
 		env.setCurrent(fac.wrapTerm(parsed));
 		return true;
+	}
+
+	private void initialize() {
+		// FIXME this must be cleaned
+		PureFactory pf = ((WrappedATermFactory) factory).getFactory();
+		ParseTableManager ptm = new ParseTableManager(pf);
+
+		ParseTable pt;
+		try {
+				pt = ptm.loadFromFile("data/Stratego.tbl");
+				StrategoSGLR = new SGLR(pf, pt);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InvalidParseTableException e) {
+			e.printStackTrace();
+		}
 	}
 
 }

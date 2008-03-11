a0aa4f71052ad125cb239d34c35f4d5af868b1ec
Karl Trygve Kalleberg <karltk@gmail.com>
2008-03-11 17:04:35 +0000

    E-mail updates.
    svn path=/spoofax/trunk/spoofax/org.spoofax.jsglr/; revision=17537

--- org.spoofax.jsglr/src/org/spoofax/jsglr/FatalException.java
+++ org.spoofax.jsglr/src/org/spoofax/jsglr/FatalException.java
@@ -1,7 +1,7 @@
 /*
  * Created on 17.apr.2006
  *
- * Copyright (c) 2005, Karl Trygve Kalleberg <karltk@ii.uib.no>
+ * Copyright (c) 2005, Karl Trygve Kalleberg <karltk near strategoxt.org>
  * 
  * Licensed under the GNU General Public License, v2
  */
--- org.spoofax.jsglr/src/org/spoofax/jsglr/Main.java
+++ org.spoofax.jsglr/src/org/spoofax/jsglr/Main.java
@@ -1,7 +1,7 @@
 /*
  * Created on 03.des.2005
  *
- * Copyright (c) 2005, Karl Trygve Kalleberg <karltk@ii.uib.no>
+ * Copyright (c) 2005, Karl Trygve Kalleberg <karltk near strategoxt.org>
  * 
  * Licensed under the GNU Lesser General Public License, v2.1
  */

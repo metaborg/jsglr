c07b8102fde7fdc92327fb2151c14ed12b7be3a5
Lennart Kats <lclkats@gmail.com>
2009-02-24 10:37:07 +0000

    JSGLR_open_parsetable caching.
    svn path=/spoofax/trunk/spoofax/org.spoofax.interpreter.library.jsglr/; revision=18439

--- org.spoofax.interpreter.library.jsglr/src/org/spoofax/interpreter/library/jsglr/JSGLR_open_parsetable.java
+++ org.spoofax.interpreter.library.jsglr/src/org/spoofax/interpreter/library/jsglr/JSGLR_open_parsetable.java
@@ -1,16 +1,24 @@
 package org.spoofax.interpreter.library.jsglr;
 
-import org.spoofax.interpreter.core.IContext;
-import org.spoofax.interpreter.core.InterpreterException;
+import java.util.WeakHashMap;
+
 import org.spoofax.interpreter.adapter.aterm.WrappedATerm;
 import org.spoofax.interpreter.adapter.aterm.WrappedATermFactory;
+import org.spoofax.interpreter.core.IContext;
+import org.spoofax.interpreter.core.InterpreterException;
 import org.spoofax.interpreter.stratego.Strategy;
+import org.spoofax.interpreter.terms.IStrategoInt;
 import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.spoofax.interpreter.terms.TermConverter;
 import org.spoofax.jsglr.InvalidParseTableException;
 import org.spoofax.jsglr.ParseTable;
 
 public class JSGLR_open_parsetable extends JSGLRPrimitive {
+    
+    // TODO: Do (non-static) caching at the IContext level
+    
+    private final WeakHashMap<IStrategoTerm, IStrategoInt> parseTableCache =
+        new WeakHashMap<IStrategoTerm, IStrategoInt>();
     
     private final TermConverter termConverter;
 
@@ -23,14 +31,25 @@ public class JSGLR_open_parsetable extends JSGLRPrimitive {
 	@Override
 	public boolean call(IContext env, Strategy[] svars, IStrategoTerm[] tvars)
 			throws InterpreterException {
-		if(!(tvars[0] instanceof WrappedATerm))
-			tvars[0] = termConverter.convert(tvars[0]);
+	    
+	    IStrategoInt cached = parseTableCache.get(tvars[0]);
+	    if (cached != null) {
+	        env.setCurrent(cached);
+	        return true;
+	    }
+	    
+	    WrappedATerm tableTerm = tvars[0] instanceof WrappedATerm
+	            ? (WrappedATerm) tvars[0]
+	            : (WrappedATerm) termConverter.convert(tvars[0]);
 		
 		JSGLRLibrary lib = getLibrary(env);
 		try {
-			ParseTable pt = lib.getParseTableManager().loadFromTerm(((WrappedATerm)tvars[0]).getATerm());
-			env.setCurrent(env.getFactory().makeInt(lib.addParseTable(pt)));
-		} catch(InvalidParseTableException e) {
+			ParseTable pt = lib.getParseTableManager().loadFromTerm(tableTerm.getATerm());
+			IStrategoInt result = env.getFactory().makeInt(lib.addParseTable(pt));
+            
+			env.setCurrent(result);			
+			parseTableCache.put(tvars[0], result);
+		} catch (InvalidParseTableException e) {
 			e.printStackTrace();
 			return false;
 		}

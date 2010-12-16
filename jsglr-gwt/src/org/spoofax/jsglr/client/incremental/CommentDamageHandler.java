d517124b41876997e4944c32257c4c038d486a74
Karl Trygve Kalleberg <karltk@gmail.com>
2010-12-16 15:06:12 +0000

    Support line comments in CommentDamageHandler.
    svn path=/spoofax/branches/jsglr-gwt/; revision=21561

--- jsglr-gwt/src/org/spoofax/jsglr/client/incremental/CommentDamageHandler.java
+++ jsglr-gwt/src/org/spoofax/jsglr/client/incremental/CommentDamageHandler.java
@@ -1,11 +1,11 @@
 package org.spoofax.jsglr.client.incremental;
 
-import static java.lang.Math.min;
+import static java.lang.Math.max;
 import static org.spoofax.jsglr.client.incremental.IncrementalSGLR.DEBUG;
 
 /**
  * A helper class that expands the damage region
- * for incremental parsing if block comments
+ * for incremental parsing if block or line comments
  * are involved.
  * 
  * @author Lennart Kats <lennart add lclnet.nl>
@@ -13,6 +13,8 @@ import static org.spoofax.jsglr.client.incremental.IncrementalSGLR.DEBUG;
 public class CommentDamageHandler {
 	
 	public static final CommentDamageHandler C_STYLE = new CommentDamageHandler("/*", "*/");
+	
+	public static final CommentDamageHandler ONLY_LINE_COMMENTS = new CommentDamageHandler(null, null);
 
 	private final String commentStart;
 	
@@ -25,7 +27,11 @@ public class CommentDamageHandler {
 	
 	public int getExpandedDamageRegionEnd(String input, int damageStart, int damageEnd) {
 		// Move back to '/' if user just entered '*'
-		damageStart = min(0, damageStart - commentStart.length() + 1);
+		damageStart = max(0, damageStart - commentStart.length() + 1);
+		// Move to following newline, in case line comments are involved
+		damageEnd = max(damageEnd, input.indexOf('\n', damageEnd));
+		
+		if (commentStart == null) return damageEnd; 
 		
 		int commentStartOffset = input.lastIndexOf(commentStart, damageEnd);
 		if (commentStartOffset >= damageStart) {

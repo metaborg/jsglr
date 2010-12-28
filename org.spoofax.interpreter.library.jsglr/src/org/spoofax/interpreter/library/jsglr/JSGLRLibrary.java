66c2037a4bcc0cf5053940d5d9bdfb6490648f42
Lennart Kats <lclkats@gmail.com>
2010-12-28 14:15:54 +0000

    refactoring
    svn path=/spoofax/branches/spoofax-new-terms/spoofax/org.spoofax.interpreter.library.jsglr/; revision=21590

--- org.spoofax.interpreter.library.jsglr/src/org/spoofax/interpreter/library/jsglr/JSGLRLibrary.java
+++ org.spoofax.interpreter.library.jsglr/src/org/spoofax/interpreter/library/jsglr/JSGLRLibrary.java
@@ -8,7 +8,7 @@ import org.spoofax.interpreter.adapter.aterm.WrappedATermFactory;
 import org.spoofax.interpreter.library.AbstractStrategoOperatorRegistry;
 import org.spoofax.interpreter.terms.ITermFactory;
 import org.spoofax.jsglr.client.ParseTable;
-import org.spoofax.jsglr.client.ParseTableManager;
+import org.spoofax.jsglr.io.ParseTableManager;
 
 import aterm.ATermFactory;
 

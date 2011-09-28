4c483ebcadb2b74d567eab88bc4524e0f7a8fb33
MaartjeDeJonge <maartjedejonge@gmail.com>
2011-09-28 12:45:30 +0000

    svn path=/spoofax/trunk/spoofax/org.spoofax.interpreter.library.jsglr/; revision=23397

--- org.spoofax.interpreter.library.jsglr/src/org/spoofax/interpreter/library/jsglr/origin/SSL_EXT_enable_desugared_origins.java
+++ org.spoofax.interpreter.library.jsglr/src/org/spoofax/interpreter/library/jsglr/origin/SSL_EXT_enable_desugared_origins.java
@@ -1,6 +1,7 @@
 package org.spoofax.interpreter.library.jsglr.origin;
 
 import org.spoofax.interpreter.core.IContext;
+import org.spoofax.interpreter.core.Tools;
 import org.spoofax.interpreter.library.AbstractPrimitive;
 import org.spoofax.interpreter.stratego.Strategy;
 import org.spoofax.interpreter.terms.IStrategoTerm;
@@ -14,18 +15,19 @@ import org.spoofax.terms.attachments.OriginTermFactory;
 public class SSL_EXT_enable_desugared_origins extends AbstractPrimitive {
     
     public SSL_EXT_enable_desugared_origins() {
-        super("SSL_EXT_enable_desugared_origins", 0, 0);
+        super("SSL_EXT_enable_desugared_origins", 0, 1);
     }
 
     @Override
     public boolean call(IContext env, Strategy[] svars, IStrategoTerm[] tvars) {
     	ITermFactory factory = env.getFactory();
+    	boolean enable = Tools.asJavaInt(tvars[0]) != 0;
     	if (factory instanceof OriginTermFactory){
-       		((OriginTermFactory)env.getFactory()).setAssignDesugaredOrigins(true);    		
+       		((OriginTermFactory)env.getFactory()).setAssignDesugaredOrigins(enable);    		
     	}
-    	else{
+    	else if (enable){
     		OriginTermFactory originFactory = new ImploderOriginTermFactory(factory);
-    		originFactory.setAssignDesugaredOrigins(true);
+    		originFactory.setAssignDesugaredOrigins(enable);
     		env.setFactory(originFactory);
     	}
     	return true;

edb6e625e2e1b79ea11fc387cdec1387626dc076
Karl Trygve Kalleberg <karltk@gmail.com>
2011-12-12 15:23:53 +0000

    Updated headers.
    svn path=/spoofax/trunk/spoofax/org.spoofax.interpreter.library.jsglr/; revision=23756

--- org.spoofax.interpreter.library.jsglr/src/org/spoofax/interpreter/library/jsglr/JSGLRPrimitive.java
+++ org.spoofax.interpreter.library.jsglr/src/org/spoofax/interpreter/library/jsglr/JSGLRPrimitive.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2005-2011, Karl Trygve Kalleberg <karltk near strategoxt dot org>
+ * 
+ * Licensed under the GNU Lesser General Public License, v2.1
+ */
 package org.spoofax.interpreter.library.jsglr;
 
 import org.spoofax.interpreter.core.IContext;
--- org.spoofax.interpreter.library.jsglr/src/org/spoofax/interpreter/library/jsglr/JSGLR_recover_parse_string.java
+++ org.spoofax.interpreter.library.jsglr/src/org/spoofax/interpreter/library/jsglr/JSGLR_recover_parse_string.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2005-2011, Karl Trygve Kalleberg <karltk near strategoxt dot org>
+ * 
+ * Licensed under the GNU Lesser General Public License, v2.1
+ */
 package org.spoofax.interpreter.library.jsglr;
 
 import static org.spoofax.interpreter.core.Tools.asJavaString;

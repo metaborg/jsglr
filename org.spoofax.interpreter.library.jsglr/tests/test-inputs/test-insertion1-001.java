aaebc9b599dce430efe92c6ef38cb964926b51b4
MaartjeDeJonge <maartjedejonge@gmail.com>
2012-07-09 10:19:22 +0000

    svn path=/spoofax/trunk/spoofax/org.spoofax.interpreter.library.jsglr/; revision=25180

---
+++ org.spoofax.interpreter.library.jsglr/tests/test-inputs/test-insertion1-001.java
@@ -0,0 +1,9 @@
+package tt;
+
+public class Example {
+	private void m(){
+		int v = 10;
+		v+= 10;
+		System.out.println(v);
+	}
+}
---
+++ org.spoofax.interpreter.library.jsglr/tests/test-inputs/test-move1-005.java
@@ -0,0 +1,13 @@
+package tt;
+
+public class Example {
+	private void m(){
+		int v = 10;
+		if(v > 5){
+			v++;
+		}
+		v+= 10;
+		v += 11;
+		System.out.println(v);
+	}
+}

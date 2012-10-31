0d11e69d3fc650572fa4bbd5b352c7cc13850ce8
SebastianErdweg <seba@informatik.uni-marburg.de>
2012-10-31 23:58:57 +0000

    drop simple2 test suite
    svn path=/spoofax/branches/jsglr-layout-merge/; revision=25646

--- org.spoofax.jsglr/test/org/spoofax/jsglr/tests/layout/AllLayoutTests.java
+++ org.spoofax.jsglr/test/org/spoofax/jsglr/tests/layout/AllLayoutTests.java
@@ -3,12 +3,11 @@ package org.spoofax.jsglr.tests.layout;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-public class AllTests {
+public class AllLayoutTests {
 
   public static Test suite() {
-    TestSuite suite = new TestSuite(AllTests.class.getName());
+    TestSuite suite = new TestSuite(AllLayoutTests.class.getName());
     //$JUnit-BEGIN$
-    suite.addTestSuite(TestSimple2.class);
     suite.addTestSuite(TestHaskell.class);
     suite.addTestSuite(TestHaskellDoaitse.class);
     //$JUnit-END$

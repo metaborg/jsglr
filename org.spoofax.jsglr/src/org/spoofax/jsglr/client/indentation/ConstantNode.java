e6291c2242eb365f594a9d32ed4b36379d074886
Moritz Lichter <lichtemo@googlemail.com>
2013-05-26 18:58:18 +0200

    Merged with compiled constraints.

---
+++ org.spoofax.jsglr/src/org/spoofax/jsglr/client/indentation/ConstantNode.java
@@ -0,0 +1,32 @@
+package org.spoofax.jsglr.client.indentation;
+
+import java.util.Map;
+
+import org.spoofax.jsglr.client.AbstractParseNode;
+
+public class ConstantNode implements IntegerNode {
+  
+  private final int value;
+  
+  public ConstantNode(int value) {
+    super();
+    this.value = value;
+  }
+
+  @Override
+  public Integer evaluate(AbstractParseNode[] kids, Map<String, Object> env,
+      boolean parseTime) {
+    return this.value;
+  }
+
+  @Override
+  public String getCompiledCode(LocalVariableManager manager, boolean atParseTime) {
+    return Integer.toString(this.value);
+  }
+
+  @Override
+  public InvokeState getInvokeState(boolean atParseTime) {
+    return InvokeState.SAFELY_INVOKABLE;
+  }
+
+}
---
+++ org.spoofax.jsglr/src/org/spoofax/jsglr/client/indentation/IntegerNode.java
@@ -0,0 +1,4 @@
+package org.spoofax.jsglr.client.indentation;
+
+public interface IntegerNode extends CompilableLayoutNode<Integer> {
+}

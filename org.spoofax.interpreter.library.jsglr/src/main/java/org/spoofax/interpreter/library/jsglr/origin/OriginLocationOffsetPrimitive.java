fa17fae0bb8552b25c4227ecfdd4df9b168b7398
Gabriel Konat <gabrielkonat@gmail.com>
2013-06-26 19:41:29 -0700

    Added primitives for getting and setting the imploder attachment on terms.

---
+++ org.spoofax.interpreter.library.jsglr/src/main/java/org/spoofax/interpreter/library/jsglr/origin/OriginLocationOffsetPrimitive.java
@@ -0,0 +1,44 @@
+package org.spoofax.interpreter.library.jsglr.origin;
+
+import org.spoofax.interpreter.core.IContext;
+import org.spoofax.interpreter.library.AbstractPrimitive;
+import org.spoofax.interpreter.stratego.Strategy;
+import org.spoofax.interpreter.terms.IStrategoTerm;
+import org.spoofax.interpreter.terms.ITermFactory;
+import org.spoofax.jsglr.client.imploder.IToken;
+import org.spoofax.jsglr.client.imploder.ImploderAttachment;
+import org.spoofax.terms.attachments.OriginAttachment;
+
+/**
+ * @author Lennart Kats <lennart add lclnet.nl>
+ */
+public class OriginLocationOffsetPrimitive extends AbstractPrimitive {
+
+	public OriginLocationOffsetPrimitive() {
+		super("SSL_EXT_origin_location_offset", 0, 1);
+	}
+
+	@Override
+	public final boolean call(IContext env, Strategy[] svars, IStrategoTerm[] tvars) {
+		final ITermFactory factory = env.getFactory();
+		final IStrategoTerm term = env.current();
+
+		ImploderAttachment imploder = ImploderAttachment.get(term);
+		if(imploder == null) {
+			final IStrategoTerm originTerm = OriginAttachment.getOrigin(term);
+			imploder = ImploderAttachment.get(originTerm);
+		}
+		if(imploder == null)
+			return false;
+
+		final IToken left = imploder.getLeftToken();
+		final IToken right = imploder.getRightToken();
+		final String filename = left.getTokenizer().getFilename();
+
+		env.setCurrent(factory.makeTuple(factory.makeString(filename), factory.makeInt(left.getLine()),
+			factory.makeInt(left.getColumn()), factory.makeInt(left.getStartOffset()),
+			factory.makeInt(right.getEndOffset())));
+		return true;
+	}
+
+}

de59a31e84910c856dba29331d8ac7797c975f4d
Jasper Denkers <jasperdenkers@gmail.com>
2017-09-12 11:34:46 +0200

    Naive Elkhound stack node implementation for fair comparison in benchmarks

--- org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/stack/elkhound/ElkhoundStackNode.java
+++ org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/stack/elkhound/ElkhoundStackNode.java
@@ -1,25 +1,20 @@
 package org.spoofax.jsglr2.stack.elkhound;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import org.spoofax.jsglr2.parsetable.IState;
 import org.spoofax.jsglr2.stack.AbstractStackNode;
 import org.spoofax.jsglr2.stack.EmptyStackPath;
 import org.spoofax.jsglr2.stack.NonEmptyStackPath;
 import org.spoofax.jsglr2.stack.StackLink;
 import org.spoofax.jsglr2.stack.StackPath;
-import org.spoofax.jsglr2.util.iterators.SingleElementIterable;
-import org.spoofax.jsglr2.util.iterators.SingleElementWithListIterable;
 
 public class ElkhoundStackNode<ParseForest> extends AbstractStackNode<ParseForest> {
 
     // Directed to the initial stack node
-    private StackLink<ElkhoundStackNode<ParseForest>, ParseForest> firstLinkOut;
-    private ArrayList<StackLink<ElkhoundStackNode<ParseForest>, ParseForest>> otherLinksOut;
+    private ArrayList<StackLink<ElkhoundStackNode<ParseForest>, ParseForest>> linksOut = new ArrayList<StackLink<ElkhoundStackNode<ParseForest>, ParseForest>>();
     
     // Directed from the initial stack node
-    private StackLink<ElkhoundStackNode<ParseForest>, ParseForest> firstLinkIn;
-    private ArrayList<StackLink<ElkhoundStackNode<ParseForest>, ParseForest>> otherLinksIn;
+    private ArrayList<StackLink<ElkhoundStackNode<ParseForest>, ParseForest>> linksIn = new ArrayList<StackLink<ElkhoundStackNode<ParseForest>, ParseForest>>();;
     
 	public int deterministicDepth;
 	
@@ -29,42 +24,21 @@ public class ElkhoundStackNode<ParseForest> extends AbstractStackNode<ParseFores
 	}
     
     public Iterable<StackLink<ElkhoundStackNode<ParseForest>, ParseForest>> getLinksOut() {
-        if (firstLinkOut == null)
-            return Collections.EMPTY_LIST;
-        else {
-            if (otherLinksOut == null)
-                return new SingleElementIterable<StackLink<ElkhoundStackNode<ParseForest>, ParseForest>>(firstLinkOut);
-            else
-                return new SingleElementWithListIterable<StackLink<ElkhoundStackNode<ParseForest>, ParseForest>>(firstLinkOut, otherLinksOut);
-        }
+        return linksOut;
     }
     
     public Iterable<StackLink<ElkhoundStackNode<ParseForest>, ParseForest>> getLinksIn() {
-        if (firstLinkIn == null)
-            return Collections.EMPTY_LIST;
-        else {
-            if (otherLinksIn == null)
-                return new SingleElementIterable<StackLink<ElkhoundStackNode<ParseForest>, ParseForest>>(firstLinkIn);
-            else
-                return new SingleElementWithListIterable<StackLink<ElkhoundStackNode<ParseForest>, ParseForest>>(firstLinkIn, otherLinksIn);
-        }
+        return linksIn;
     }
     
     public StackLink<ElkhoundStackNode<ParseForest>, ParseForest> addOutLink(StackLink<ElkhoundStackNode<ParseForest>, ParseForest> link) {
-        if (firstLinkOut == null)
-            firstLinkOut = link;
-        else {
-            if (otherLinksOut == null)
-                otherLinksOut = new ArrayList<StackLink<ElkhoundStackNode<ParseForest>, ParseForest>>();
-            
-            otherLinksOut.add(link);
-        }
+        linksOut.add(link);
         
         link.to.addInLink(link);
         
-        if (otherLinksOut == null) { // This means the first link is just added.
+        if (linksOut.size() == 1) { // This means the first link is just added.
             deterministicDepth = link.to.deterministicDepth + 1;
-        } else if (otherLinksOut.size() == 1) { // The second link is added; this means non-determinism
+        } else if (linksOut.size() == 2) { // The second link is added; this means non-determinism
             deterministicDepth = 0;
             
             for (StackLink<ElkhoundStackNode<ParseForest>, ParseForest> linkIn : getLinksIn())
@@ -81,14 +55,7 @@ public class ElkhoundStackNode<ParseForest> extends AbstractStackNode<ParseFores
 	}
     
     private void addInLink(StackLink<ElkhoundStackNode<ParseForest>, ParseForest> link) {
-        if (firstLinkIn == null)
-            firstLinkIn = link;
-        else {
-            if (otherLinksIn == null)
-                otherLinksIn = new ArrayList<StackLink<ElkhoundStackNode<ParseForest>, ParseForest>>();
-            
-            otherLinksIn.add(link);
-        }
+        linksIn.add(link);
     }
     
     public void resetDeterministicDepth(int deterministicDepth) {
@@ -100,32 +67,27 @@ public class ElkhoundStackNode<ParseForest> extends AbstractStackNode<ParseFores
         }
     }
 	
-	public boolean allLinksRejected() {
-	    if (firstLinkOut == null)
+    public boolean allLinksRejected() {
+        if (linksOut.isEmpty())
             return false;
         
-        if (!firstLinkOut.isRejected())
-            return false;
-        
-        if (otherLinksOut != null) {
-            for (StackLink<ElkhoundStackNode<ParseForest>, ParseForest> link : otherLinksOut) {
-                if (!link.isRejected())
-                    return false;
-            }
+        for (StackLink<ElkhoundStackNode<ParseForest>, ParseForest> link : linksOut) {
+            if (!link.isRejected())
+                return false;
         }
         
         return true;
-	}
+    }
     
     public StackPath<ElkhoundStackNode<ParseForest>, ParseForest> findDeterministicPathOfLength(int length) {
         if (length == 0)
             return new EmptyStackPath<ElkhoundStackNode<ParseForest>, ParseForest>(this);
-        else if (length == 1)
-            return new NonEmptyStackPath<ElkhoundStackNode<ParseForest>, ParseForest>(firstLinkOut);
         else {
-            StackPath<ElkhoundStackNode<ParseForest>, ParseForest> pathAfterOwnLink = firstLinkOut.to.findDeterministicPathOfLength(length - 1);
+            StackLink<ElkhoundStackNode<ParseForest>, ParseForest> onlyOwnLink = this.linksOut.get(0);
             
-            return new NonEmptyStackPath<ElkhoundStackNode<ParseForest>, ParseForest>(firstLinkOut, pathAfterOwnLink);
+            StackPath<ElkhoundStackNode<ParseForest>, ParseForest> pathAfterOwnLink = onlyOwnLink.to.findDeterministicPathOfLength(length - 1);
+            
+            return new NonEmptyStackPath<ElkhoundStackNode<ParseForest>, ParseForest>(onlyOwnLink, pathAfterOwnLink);
         }
     }
 	

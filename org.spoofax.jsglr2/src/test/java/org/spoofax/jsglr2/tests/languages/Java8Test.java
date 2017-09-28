2c2fd74db5b1435c777731c7e5776bd0ef4b9db3
Jasper Denkers <jasperdenkers@gmail.com>
2017-09-28 16:58:16 +0200

    Remove MiniJava stuff

--- org.spoofax.jsglr2/src/test/java/org/spoofax/jsglr2/tests/languages/Java8Test.java
+++ org.spoofax.jsglr2/src/test/java/org/spoofax/jsglr2/tests/languages/Java8Test.java
@@ -18,15 +18,15 @@ public class Java8Test extends BaseTestWithJSGLR1 implements WithParseTable {
     
     @Test
     public void testSampleProgramByExpectedAST() throws ParseError, ParseTableReadException, IOException {
-        String sampleProgram = getFileAsString("MiniJava/sampleProgram.txt");
-        IStrategoTerm expectedAST = getFileAsAST("MiniJava/sampleProgram_Java8.ast");
+        String sampleProgram = getFileAsString("Java/sampleProgram.txt");
+        IStrategoTerm expectedAST = getFileAsAST("Java/sampleProgram.ast");
         
         testParseSuccessByAstString(sampleProgram, expectedAST.toString());
     }
     
     @Test
     public void testSampleProgramByJSGLR1() throws ParseError, ParseTableReadException, IOException {
-        String sampleProgram = getFileAsString("MiniJava/sampleProgram.txt");
+        String sampleProgram = getFileAsString("Java/sampleProgram.txt");
         
         testParseSuccessByJSGLR(sampleProgram);
     }

5a5943b8f3fafec6c338b6b4580c5f94e236f1c8
Jasper Denkers <jasperdenkers@gmail.com>
2017-11-11 12:09:07 +0100

    Refactor character classes tests, better error messages

--- org.spoofax.jsglr2/src/test/java/org/spoofax/jsglr2/tests/util/CharacterClassTest.java
+++ org.spoofax.jsglr2/src/test/java/org/spoofax/jsglr2/tests/util/CharacterClassTest.java
@@ -2,6 +2,8 @@ package org.spoofax.jsglr2.tests.util;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.function.Predicate;
+
 import org.junit.Test;
 import org.spoofax.jsglr2.characters.ICharacterClassFactory;
 import org.spoofax.jsglr2.characters.ICharacters;
@@ -15,43 +17,49 @@ public class CharacterClassTest {
 
     ICharacters x = factory.fromSingle(120);
 
-    @Test public void testLowerCaseLettersRange() {
+    private void testCharacterClass(ICharacters characters, Predicate<Integer> contains) {
         for(int i = 0; i <= 255; i++) {
-            assertEquals(az.containsCharacter(ICharacters.charToByte((char) i)), 97 <= i && i <= 122);
+            assertEquals("Character " + i + " ('" + ICharacters.intToString(i) + "') for characters "
+                + characters.toString() + ":", contains.test(i),
+                characters.containsCharacter(ICharacters.charToByte((char) i)));
         }
+    }
+
+    @Test public void testLowerCaseLettersRange() {
+        testCharacterClass(az, character -> {
+            return 97 <= character && character <= 122;
+        });
 
         assertEquals(az.containsCharacter(ICharacters.charToByte('a')), true);
         assertEquals(az.containsCharacter(ICharacters.charToByte('A')), false);
     }
 
     @Test public void testUppercaseCaseLettersRange() {
-        for(int i = 0; i <= 255; i++) {
-            assertEquals(AZ.containsCharacter(ICharacters.charToByte((char) i)), 65 <= i && i <= 90);
-        }
+        testCharacterClass(AZ, character -> {
+            return 65 <= character && character <= 90;
+        });
     }
 
     @Test public void testLettersUnionRange() {
-        for(int i = 0; i <= 255; i++) {
-            ICharacters letters = factory.union(az, AZ);
+        ICharacters letters = factory.union(az, AZ);
 
-            assertEquals(letters.containsCharacter(ICharacters.charToByte((char) i)),
-                65 <= i && i <= 90 || 97 <= i && i <= 122);
-        }
+        testCharacterClass(letters, character -> {
+            return 65 <= character && character <= 90 || 97 <= character && character <= 122;
+        });
     }
 
     @Test public void testSingletonRange() {
-        for(int i = 0; i <= 255; i++) {
-            assertEquals(x.containsCharacter(ICharacters.charToByte((char) i)), i == 120);
-        }
+        testCharacterClass(x, character -> {
+            return character == 120;
+        });
     }
 
     @Test public void testSingletonRangeUnion() {
         ICharacters characters = factory.union(x, AZ);
 
-        for(int i = 0; i <= 255; i++) {
-            assertEquals(characters.containsCharacter(ICharacters.charToByte((char) i)),
-                65 <= i && i <= 90 || i == 120);
-        }
+        testCharacterClass(characters, character -> {
+            return 65 <= character && character <= 90 || character == 120;
+        });
 
         assertEquals(characters.containsCharacter(ICharacters.charToByte('a')), false);
         assertEquals(characters.containsCharacter(ICharacters.charToByte('B')), true);
@@ -63,17 +71,17 @@ public class CharacterClassTest {
 
         assertEquals(characters.containsEOF(), true);
 
-        for(int i = 0; i <= 255; i++) {
-            assertEquals(characters.containsCharacter(ICharacters.charToByte((char) i)), false);
-        }
+        testCharacterClass(characters, character -> {
+            return false;
+        });
     }
 
-    @Test public void testRangeEOnion() {
+    @Test public void testRangeEOFnion() {
         ICharacters characters = factory.union(az, factory.fromEOF());
 
-        for(int i = 0; i <= 255; i++) {
-            assertEquals(characters.containsCharacter(ICharacters.charToByte((char) i)), 97 <= i && i <= 122);
-        }
+        testCharacterClass(characters, character -> {
+            return 97 <= character && character <= 122;
+        });
 
         assertEquals(characters.containsEOF(), true);
     }

86c9436929acd0e4384c00fe64d5c9ef863cb904
Jasper Denkers <jasperdenkers@gmail.com>
2017-11-15 09:40:11 +0100

    Document what the for actor collections do

--- org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/parser/IForActorStacks.java
+++ org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/parser/IForActorStacks.java
@@ -2,6 +2,13 @@ package org.spoofax.jsglr2.parser;
 
 import org.spoofax.jsglr2.stack.AbstractStackNode;
 
+/*
+ * Collection of stacks that the parser operates on during a parse round. It is cleared on the start of each round and
+ * filled with the collection of active stacks at the beginning of the new round (see Parser::parseCharacter). Stacks
+ * are removed one by one and during a parse round new stacks can be added after reducing. Removing should return stacks
+ * with a state that is rejectable last. In the original SGLR algorithm such stacks are in for-actor-delayed. The
+ * rejectable stacks should be returned in a certain order (see P9707 Section 8.4).
+ */
 public interface IForActorStacks<StackNode extends AbstractStackNode<?>> {
 
     void add(StackNode stack);

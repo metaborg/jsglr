6a43f37dfdea033cd2e0bbebb85ebf5780d8dc09
Jasper Denkers <jasperdenkers@gmail.com>
2017-11-30 14:25:47 +0100

    Try with lists

--- org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/states/MultipleActionGroupsForRange.java
+++ org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/states/MultipleActionGroupsForRange.java
@@ -2,15 +2,11 @@ package org.spoofax.jsglr2.states;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
-import java.util.NoSuchElementException;
 
-import org.spoofax.jsglr2.actions.ActionType;
 import org.spoofax.jsglr2.actions.ActionsPerCharacterClass;
 import org.spoofax.jsglr2.actions.IAction;
 import org.spoofax.jsglr2.actions.IReduce;
-import org.spoofax.jsglr2.actions.IReduceLookahead;
 import org.spoofax.jsglr2.parser.Parse;
 import org.spoofax.jsglr2.util.Range;
 
@@ -26,10 +22,8 @@ class MultipleActionGroupsForRange extends ActionsForRange {
     public List<IAction> getActions() {
         List<IAction> res = new ArrayList<>();
 
-        for(ActionsPerCharacterClass actionsPerCharacterClass : actionsPerCharacterClasses) {
-            for(IAction action : actionsPerCharacterClass.actions)
-                res.add(action);
-        }
+        for(ActionsPerCharacterClass actionsPerCharacterClass : actionsPerCharacterClasses)
+            res.addAll(actionsPerCharacterClass.actions);
 
         return res;
     }
@@ -38,86 +32,104 @@ class MultipleActionGroupsForRange extends ActionsForRange {
         if(!range.contains(character))
             return Collections.EMPTY_LIST;
 
-        return () -> new Iterator<IAction>() {
-            int characterClassIndex = 0, actionInCharacterClassIndex = 0;
+        List<IAction> actions = new ArrayList<>();
 
-            @Override public boolean hasNext() {
-                while(characterClassIndex < actionsPerCharacterClasses.length
-                    && actionInCharacterClassIndex < actionsPerCharacterClasses[characterClassIndex].actions.size()) {
-                    actionInCharacterClassIndex++;
+        for(ActionsPerCharacterClass actionsPerCharacterClass : actionsPerCharacterClasses) {
+            if(actionsPerCharacterClass.appliesTo(character))
+                actions.addAll(actionsPerCharacterClass.actions);
+        }
 
-                    if(actionInCharacterClassIndex >= actionsPerCharacterClasses[characterClassIndex].actions.size()) {
-                        characterClassIndex++;
-                        actionInCharacterClassIndex = 0;
-                    }
-                }
+        return actions;
 
-                return characterClassIndex < actionsPerCharacterClasses.length
-                    && actionInCharacterClassIndex < actionsPerCharacterClasses[characterClassIndex].actions.size();
-            }
-
-            @Override public IAction next() {
-                if(!hasNext()) {
-                    throw new NoSuchElementException();
-                }
-
-                int nextActionInCharacterClassIndex = actionInCharacterClassIndex++;
-
-                if(actionInCharacterClassIndex >= actionsPerCharacterClasses[characterClassIndex].actions.size()) {
-                    characterClassIndex++;
-                    actionInCharacterClassIndex = 0;
-                }
-
-                return actionsPerCharacterClasses[characterClassIndex].actions.get(nextActionInCharacterClassIndex);
-            }
-        };
+        // return () -> new Iterator<IAction>() {
+        // int characterClassIndex = 0, actionInCharacterClassIndex = 0;
+        //
+        // @Override public boolean hasNext() {
+        // while(characterClassIndex < actionsPerCharacterClasses.length
+        // && actionInCharacterClassIndex < actionsPerCharacterClasses[characterClassIndex].actions.size()) {
+        // actionInCharacterClassIndex++;
+        //
+        // if(actionInCharacterClassIndex >= actionsPerCharacterClasses[characterClassIndex].actions.size()) {
+        // characterClassIndex++;
+        // actionInCharacterClassIndex = 0;
+        // }
+        // }
+        //
+        // return characterClassIndex < actionsPerCharacterClasses.length
+        // && actionInCharacterClassIndex < actionsPerCharacterClasses[characterClassIndex].actions.size();
+        // }
+        //
+        // @Override public IAction next() {
+        // if(!hasNext()) {
+        // throw new NoSuchElementException();
+        // }
+        //
+        // int nextActionInCharacterClassIndex = actionInCharacterClassIndex++;
+        //
+        // if(actionInCharacterClassIndex >= actionsPerCharacterClasses[characterClassIndex].actions.size()) {
+        // characterClassIndex++;
+        // actionInCharacterClassIndex = 0;
+        // }
+        //
+        // return actionsPerCharacterClasses[characterClassIndex].actions.get(nextActionInCharacterClassIndex);
+        // }
+        // };
     }
 
     public Iterable<IReduce> getReduceActions(Parse parse) {
         if(!range.contains(parse.currentChar))
             return Collections.EMPTY_LIST;
 
-        return () -> new Iterator<IReduce>() {
-            int characterClassIndex = 0, actionInCharacterClassIndex = 0;
+        List<IReduce> reduces = new ArrayList<>();
 
-            private boolean actionApplies(IAction action) {
-                return action.actionType() == ActionType.REDUCE || (action.actionType() == ActionType.REDUCE_LOOKAHEAD
-                    && ((IReduceLookahead) action).allowsLookahead(parse));
-            }
+        for(ActionsPerCharacterClass actionsPerCharacterClass : actionsPerCharacterClasses) {
+            if(actionsPerCharacterClass.appliesTo(parse.currentChar))
+                reduces.addAll(actionsPerCharacterClass.getReduceActions(parse));
+        }
 
-            @Override public boolean hasNext() {
-                while(characterClassIndex < actionsPerCharacterClasses.length
-                    && actionInCharacterClassIndex < actionsPerCharacterClasses[characterClassIndex].actions.size()
-                    && actionApplies(
-                        actionsPerCharacterClasses[characterClassIndex].actions.get(actionInCharacterClassIndex))) {
-                    actionInCharacterClassIndex++;
+        return reduces;
 
-                    if(actionInCharacterClassIndex >= actionsPerCharacterClasses[characterClassIndex].actions.size()) {
-                        characterClassIndex++;
-                        actionInCharacterClassIndex = 0;
-                    }
-                }
-
-                return characterClassIndex < actionsPerCharacterClasses.length
-                    && actionInCharacterClassIndex < actionsPerCharacterClasses[characterClassIndex].actions.size();
-            }
-
-            @Override public IReduce next() {
-                if(!hasNext()) {
-                    throw new NoSuchElementException();
-                }
-
-                int nextActionInCharacterClassIndex = actionInCharacterClassIndex++;
-
-                if(actionInCharacterClassIndex >= actionsPerCharacterClasses[characterClassIndex].actions.size()) {
-                    characterClassIndex++;
-                    actionInCharacterClassIndex = 0;
-                }
-
-                return (IReduce) actionsPerCharacterClasses[characterClassIndex].actions
-                    .get(nextActionInCharacterClassIndex);
-            }
-        };
+        // return () -> new Iterator<IReduce>() {
+        // int characterClassIndex = 0, actionInCharacterClassIndex = 0;
+        //
+        // private boolean actionApplies(IAction action) {
+        // return action.actionType() == ActionType.REDUCE || (action.actionType() == ActionType.REDUCE_LOOKAHEAD
+        // && ((IReduceLookahead) action).allowsLookahead(parse));
+        // }
+        //
+        // @Override public boolean hasNext() {
+        // while(characterClassIndex < actionsPerCharacterClasses.length
+        // && actionInCharacterClassIndex < actionsPerCharacterClasses[characterClassIndex].actions.size()
+        // && actionApplies(
+        // actionsPerCharacterClasses[characterClassIndex].actions.get(actionInCharacterClassIndex))) {
+        // actionInCharacterClassIndex++;
+        //
+        // if(actionInCharacterClassIndex >= actionsPerCharacterClasses[characterClassIndex].actions.size()) {
+        // characterClassIndex++;
+        // actionInCharacterClassIndex = 0;
+        // }
+        // }
+        //
+        // return characterClassIndex < actionsPerCharacterClasses.length
+        // && actionInCharacterClassIndex < actionsPerCharacterClasses[characterClassIndex].actions.size();
+        // }
+        //
+        // @Override public IReduce next() {
+        // if(!hasNext()) {
+        // throw new NoSuchElementException();
+        // }
+        //
+        // int nextActionInCharacterClassIndex = actionInCharacterClassIndex++;
+        //
+        // if(actionInCharacterClassIndex >= actionsPerCharacterClasses[characterClassIndex].actions.size()) {
+        // characterClassIndex++;
+        // actionInCharacterClassIndex = 0;
+        // }
+        //
+        // return (IReduce) actionsPerCharacterClasses[characterClassIndex].actions
+        // .get(nextActionInCharacterClassIndex);
+        // }
+        // };
     }
 
 }
--- org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/states/SingleActionGroupForRange.java
+++ org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/states/SingleActionGroupForRange.java
@@ -1,15 +1,11 @@
 package org.spoofax.jsglr2.states;
 
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
-import java.util.NoSuchElementException;
 
-import org.spoofax.jsglr2.actions.ActionType;
 import org.spoofax.jsglr2.actions.ActionsPerCharacterClass;
 import org.spoofax.jsglr2.actions.IAction;
 import org.spoofax.jsglr2.actions.IReduce;
-import org.spoofax.jsglr2.actions.IReduceLookahead;
 import org.spoofax.jsglr2.parser.Parse;
 import org.spoofax.jsglr2.util.Range;
 
@@ -34,33 +30,35 @@ class SingleActionGroupForRange extends ActionsForRange {
     }
 
     public Iterable<IReduce> getReduceActions(Parse parse) {
-        if(range.contains(parse.currentChar) && actionsPerCharacterClass.characterClass.contains(parse.currentChar))
-            return () -> new Iterator<IReduce>() {
-                int index = 0;
+        if(range.contains(parse.currentChar) && actionsPerCharacterClass.characterClass.contains(parse.currentChar)) {
+            return actionsPerCharacterClass.getReduceActions(parse);
 
-                private boolean actionApplies(IAction action) {
-                    return action.actionType() == ActionType.REDUCE
-                        || (action.actionType() == ActionType.REDUCE_LOOKAHEAD
-                            && ((IReduceLookahead) action).allowsLookahead(parse));
-                }
-
-                @Override public boolean hasNext() {
-                    // skip non-applicable actions
-                    while(index < actionsPerCharacterClass.actions.size()
-                        && !actionApplies(actionsPerCharacterClass.actions.get(index))) {
-                        index++;
-                    }
-                    return index < actionsPerCharacterClass.actions.size();
-                }
-
-                @Override public IReduce next() {
-                    if(!hasNext()) {
-                        throw new NoSuchElementException();
-                    }
-                    return (IReduce) actionsPerCharacterClass.actions.get(index++);
-                }
-            };
-        else
+            // return () -> new Iterator<IReduce>() {
+            // int index = 0;
+            //
+            // private boolean actionApplies(IAction action) {
+            // return action.actionType() == ActionType.REDUCE
+            // || (action.actionType() == ActionType.REDUCE_LOOKAHEAD
+            // && ((IReduceLookahead) action).allowsLookahead(parse));
+            // }
+            //
+            // @Override public boolean hasNext() {
+            // // skip non-applicable actions
+            // while(index < actionsPerCharacterClass.actions.size()
+            // && !actionApplies(actionsPerCharacterClass.actions.get(index))) {
+            // index++;
+            // }
+            // return index < actionsPerCharacterClass.actions.size();
+            // }
+            //
+            // @Override public IReduce next() {
+            // if(!hasNext()) {
+            // throw new NoSuchElementException();
+            // }
+            // return (IReduce) actionsPerCharacterClass.actions.get(index++);
+            // }
+            // };
+        } else
             return Collections.EMPTY_LIST;
     }
 

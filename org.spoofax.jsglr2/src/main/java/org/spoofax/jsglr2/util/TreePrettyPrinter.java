5945e3e9ee57437eb94c141df13a9478c1cde6f9
Jasper Denkers <j.denkers@tudelft.nl>
2018-09-11 14:35:28 +0200

    Pretty printing for hybrid parse forests

---
+++ org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/util/TreePrettyPrinter.java
@@ -0,0 +1,48 @@
+package org.spoofax.jsglr2.util;
+
+public class TreePrettyPrinter {
+
+	private int indent;
+	private StringBuilder builder;
+	
+    public TreePrettyPrinter() {
+    	this.indent = 0;
+    	this.builder = new StringBuilder();
+    }
+    
+    public void indent(int delta) {
+    	assert(indent + delta >= 0);
+    	
+    	indent += delta;		
+    }
+
+    public void print(String s) {
+        print(s, true);
+    }
+
+    public void print(String s, boolean indent) {
+        printIndent(indent);
+        builder.append(s);    
+    }
+
+    public void println(String s) {
+        println(s, true);
+    }
+
+    public void println(String s, boolean indent) {
+        print(s, indent);
+        builder.append('\n');
+    }
+
+    private void printIndent(boolean printIndent) {
+        if(printIndent) {
+        	for (int i = 0; i < indent; i++)
+                builder.append(' ');
+        }
+    }
+
+    public String get() {
+        return builder.toString();
+    }
+
+}

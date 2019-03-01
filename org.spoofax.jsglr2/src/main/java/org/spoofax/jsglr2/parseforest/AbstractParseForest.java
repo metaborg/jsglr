7ebe2ad80bb5888bfa1206f5833f2a9f77a3ef8c
Jasper Denkers <j.denkers@tudelft.nl>
2019-03-01 19:25:00 +0100

    Remove tokens from parse forest

--- org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/parseforest/AbstractParseForest.java
+++ org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/parseforest/AbstractParseForest.java
@@ -1,21 +1,14 @@
 package org.spoofax.jsglr2.parseforest;
 
-import org.spoofax.jsglr.client.imploder.IToken;
 import org.spoofax.jsglr2.parser.Position;
 
 public abstract class AbstractParseForest {
 
     private final Position startPosition, endPosition;
 
-    public IToken token, firstToken, lastToken;
-
     protected AbstractParseForest(Position startPosition, Position endPosition) {
         this.startPosition = startPosition;
         this.endPosition = endPosition;
-
-        this.token = null;
-        this.firstToken = null;
-        this.lastToken = null;
     }
 
     public Position getStartPosition() {

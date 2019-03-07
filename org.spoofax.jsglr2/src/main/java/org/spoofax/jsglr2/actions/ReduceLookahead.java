702d0f0376dddf86c638d5936a6c9e2a12801fdf
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2019-03-07 11:45:37 +0100

    Move org.metaborg.parsetable.actions.Reduce to org.spoofax.jsglr2.actions

--- org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/actions/ReduceLookahead.java
+++ org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/actions/ReduceLookahead.java
@@ -6,7 +6,6 @@ import org.metaborg.parsetable.IActionQuery;
 import org.metaborg.parsetable.IProduction;
 import org.metaborg.parsetable.ProductionType;
 import org.metaborg.parsetable.actions.IReduceLookahead;
-import org.metaborg.parsetable.actions.Reduce;
 import org.metaborg.parsetable.characterclasses.ICharacterClass;
 
 public class ReduceLookahead extends Reduce implements IReduceLookahead {

6635b821084bb16e14f2e5e2472a8c71eead9e88
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2019-04-03 13:49:24 +0200

    Rewrite getActions using Iterables from Guava

--- org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/incremental/IncrementalParser.java
+++ org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/incremental/IncrementalParser.java
@@ -1,9 +1,15 @@
 package org.spoofax.jsglr2.incremental;
 
 
+import static com.google.common.collect.Iterables.isEmpty;
+import static com.google.common.collect.Iterables.size;
+import static org.metaborg.util.iterators.Iterables2.stream;
 import static org.spoofax.jsglr2.incremental.IncrementalParse.NO_STATE;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import org.metaborg.parsetable.IParseTable;
@@ -104,20 +110,20 @@ public class IncrementalParser
     }
 
     @Override protected void actor(StackNode stack, Parse parse) {
-        Collection<IAction> actions = getActions(stack, parse);
+        Iterable<IAction> actions = getActions(stack, parse);
         // Break down lookahead until it is a terminal or until there is something to be shifted
         // Break down lookahead if it has no state or if there are no actions for it
         // TODO Maybe: break down lookahead if parse.multipleStates?
         while(!parse.lookahead.get().isTerminal()
-            && (lookaheadHasNoState(parse.lookahead) || actions.size() == 0 && parse.forShifter.isEmpty())) {
+            && (lookaheadHasNoState(parse.lookahead) || isEmpty(actions) && parse.forShifter.isEmpty())) {
             parse.lookahead.leftBreakdown();
             actions = getActions(stack, parse);
         }
 
-        if(actions.size() > 1)
+        if(size(actions) > 1)
             parse.multipleStates = true;
 
-        Collection<IAction> finalActions = actions;
+        Iterable<IAction> finalActions = actions;
         observing.notify(observer -> observer.actor(stack, parse, finalActions));
 
         for(IAction action : actions)
@@ -130,28 +136,22 @@ public class IncrementalParser
 
     // Inside this method, we can assume that the lookahead is a valid and complete subtree of the previous parse.
     // Else, the loop in `actor` will have broken it down
-    private Collection<IAction> getActions(StackNode stack, Parse parse) {
+    private Iterable<IAction> getActions(StackNode stack, Parse parse) {
+        // Get actions based on the lookahead terminal that `parse` will calculate in actionQueryCharacter
+        Iterable<IAction> actions = stack.state().getApplicableActions(parse);
+
         IncrementalParseForest lookahead = parse.lookahead.get();
         if(lookahead.isTerminal()) {
-            LinkedList<IAction> actions = new LinkedList<>();
-            stack.state().getApplicableActions(parse).forEach(actions::add);
             return actions;
         } else {
-            LinkedList<IAction> actions = new LinkedList<>();
-
-            // Get actions based on the lookahead terminal that `parse` will calculate in actionQueryCharacter
-            stack.state().getApplicableActions(parse).forEach(actions::add);
-
             // Split in shift and reduce actions
-            List<IShift> shiftActions = actions.stream().filter(a -> a.actionType() == ActionType.SHIFT)
+            List<IShift> shiftActions = stream(actions).filter(a -> a.actionType() == ActionType.SHIFT)
                 .map(a -> ((IShift) a)).collect(Collectors.toList());
-            List<IReduce> reduceActions = actions.stream()
-                .filter(a -> a.actionType() == ActionType.REDUCE || a.actionType() == ActionType.REDUCE_LOOKAHEAD)
-                .map(a -> ((IReduce) a)).collect(Collectors.toList());
 
-            // By default, only return the reduce actions
-            actions.clear();
-            actions.addAll(reduceActions);
+            // By default, only the reduce actions are returned
+            LinkedList<IAction> result = stream(actions)
+                .filter(a -> a.actionType() == ActionType.REDUCE || a.actionType() == ActionType.REDUCE_LOOKAHEAD)
+                .map(a -> ((IReduce) a)).collect(Collectors.toCollection(LinkedList::new));
 
             IncrementalParseNode lookaheadNode = (IncrementalParseNode) lookahead;
 
@@ -159,7 +159,7 @@ public class IncrementalParser
             boolean hasGotoShift = false;
             for(IncrementalDerivation derivation : lookaheadNode.getDerivations()) {
                 if(stack.state().id() == derivation.state.id()) {
-                    actions.add(new GotoShift(stack.state().getGotoId(derivation.production().id())));
+                    result.add(new GotoShift(stack.state().getGotoId(derivation.production().id())));
                     hasGotoShift = true;
                 }
             }
@@ -171,12 +171,12 @@ public class IncrementalParser
 
             // If lookahead has null yield and the production of lookahead matches the state of the GotoShift,
             // there is a duplicate action that can be removed (this is an optimization to avoid multipleStates == true)
-            if(!lookaheadNode.isAmbiguous() && lookaheadNode.width() == 0 && actions.size() == 2 && hasGotoShift
-                && nullReduceMatchesGotoShift(stack, (IReduce) actions.getFirst(), (GotoShift) actions.getLast())) {
-                actions.removeFirst(); // Removes the unnecessary reduce action
+            if(!lookaheadNode.isAmbiguous() && lookaheadNode.width() == 0 && result.size() == 2 && hasGotoShift
+                && nullReduceMatchesGotoShift(stack, (IReduce) result.getFirst(), (GotoShift) result.getLast())) {
+                result.removeFirst(); // Removes the unnecessary reduce action
             }
 
-            return actions;
+            return result;
         }
     }
 

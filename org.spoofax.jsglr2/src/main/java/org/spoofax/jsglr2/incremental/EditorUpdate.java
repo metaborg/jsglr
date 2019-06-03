cb0927584ecacadd3cb2a08d039ceac7bf56717f
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2019-06-03 10:40:01 +0200

    Store update type in EditorUpdate

--- org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/incremental/EditorUpdate.java
+++ org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/incremental/EditorUpdate.java
@@ -3,14 +3,26 @@ package org.spoofax.jsglr2.incremental;
 import java.util.Objects;
 
 public class EditorUpdate {
+
+    public enum Type {
+        DELETION, INSERTION, REPLACEMENT
+    }
+
     public final int deletedStart;
     public final int deletedEnd;
     public final String inserted;
+    public final Type type; // Redundant information that is calculated based on other fields, just for convenience
 
     public EditorUpdate(int deletedStart, int deletedEnd, String inserted) {
         this.deletedStart = deletedStart;
         this.deletedEnd = deletedEnd;
         this.inserted = inserted;
+        if(deletedStart == deletedEnd)
+            type = Type.INSERTION;
+        else if(inserted.length() == 0)
+            type = Type.DELETION;
+        else
+            type = Type.REPLACEMENT;
     }
 
     @Override public boolean equals(Object o) {

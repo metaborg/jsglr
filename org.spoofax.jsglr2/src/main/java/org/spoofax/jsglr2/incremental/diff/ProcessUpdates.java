ed89b2fafb7f84dafcc3bd42a1701cd30a53680e
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2019-06-03 10:26:17 +0200

    Extract delegate class ProcessUpdates from Incremental Parse
    Also, multiple editor updates will now be handled correctly.

---
+++ org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/incremental/diff/ProcessUpdates.java
@@ -0,0 +1,152 @@
+package org.spoofax.jsglr2.incremental.diff;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+import org.spoofax.jsglr2.incremental.EditorUpdate;
+import org.spoofax.jsglr2.incremental.IncrementalParse;
+import org.spoofax.jsglr2.incremental.parseforest.IncrementalParseForest;
+import org.spoofax.jsglr2.incremental.parseforest.IncrementalParseForestManager;
+import org.spoofax.jsglr2.incremental.parseforest.IncrementalParseNode;
+import org.spoofax.jsglr2.stack.IStackNode;
+
+public class ProcessUpdates<StackNode extends IStackNode> {
+
+    private final IncrementalParse<StackNode> incrementalParse;
+    private final IncrementalParseForestManager parseForestManager = new IncrementalParseForestManager();
+
+    public ProcessUpdates(IncrementalParse<StackNode> incrementalParse) {
+        this.incrementalParse = incrementalParse;
+    }
+
+    public IncrementalParseForest processUpdates(IncrementalParseForest previous, EditorUpdate... editorUpdates) {
+        return processUpdates(previous, Arrays.asList(editorUpdates));
+    }
+
+    /**
+     * Recursively processes the tree until the update site has been found. General strategy:
+     *
+     * <ul>
+     * <li>If the update is a replacement (a, b, new) with a != b: replace character a with the new string and delete
+     * characters [a+1, b>
+     * <li>If the update is an insertion (a, a, new), and
+     * <ul>
+     * <li>the insertion is at the first character: replace first character with (new, first)
+     * <li>the insertion is anywhere else: replace character a with (a, new)
+     * </ul>
+     * <li>If the update is a deletion (a, b, ""): just delete everything in range [a, b>
+     * </ul>
+     * 
+     * The slightly reasonable assumption has been made that any two consecutive updates never "touch" each other
+     * (meaning that first.end == second.start). This method will still work for two consecutive replacements, but no
+     * guarantees are made for a consecutive insertion/deletion.
+     */
+    public IncrementalParseForest processUpdates(IncrementalParseForest previous, List<EditorUpdate> editorUpdates) {
+        // Optimization: f everything is deleted, then return a tree created from the inserted string
+        if(editorUpdates.size() == 1) {
+            EditorUpdate editorUpdate = editorUpdates.get(0);
+            if(editorUpdate.deletedStart == 0 && editorUpdate.deletedEnd == previous.width()) {
+                return getParseNodeFromString(editorUpdate.inserted);
+            }
+        }
+
+        LinkedList<EditorUpdate> linkedUpdates = new LinkedList<>(editorUpdates);
+        return processUpdates(previous, 0, linkedUpdates);
+    }
+
+    private IncrementalParseForest processUpdates(IncrementalParseForest currentForest, int currentOffset,
+        LinkedList<EditorUpdate> updates) {
+        if(currentForest.isTerminal()) {
+            EditorUpdate update = updates.getFirst();
+            int deletedStartOffset = update.deletedStart;
+            int deletedEndOffset = update.deletedEnd;
+            String inserted = update.inserted;
+
+            // If there is nothing to delete (it is an insertion)
+            if(deletedStartOffset == deletedEndOffset) {
+                // If insert position is begin of string, prepend to first character
+                if(deletedStartOffset == 0 && currentOffset == deletedEndOffset) {
+                    updates.removeFirst();
+                    return newParseNodeFromChildren(getParseNodeFromString(inserted), currentForest);
+                }
+                // If insert position is NOT begin of string, append to current character
+                if(deletedStartOffset != 0 && currentOffset == deletedStartOffset - 1) {
+                    updates.removeFirst();
+                    return newParseNodeFromChildren(currentForest, getParseNodeFromString(inserted));
+                }
+                // If none of the cases applies, just return original character node
+                return currentForest;
+            }
+            // Replace first deleted character with the inserted string (if any)
+            if(deletedStartOffset == currentOffset && inserted.length() > 0) {
+                if(currentOffset == deletedEndOffset - 1)
+                    updates.removeFirst();
+                return getParseNodeFromString(inserted);
+            }
+            // Else, delete all characters within deletion range
+            if(deletedStartOffset <= currentOffset && currentOffset < deletedEndOffset) {
+                if(currentOffset == deletedEndOffset - 1)
+                    updates.removeFirst();
+                return null;
+            }
+            // If none of the cases applies, just return original character node
+            return currentForest;
+        }
+        // Use a shallow copy of the current children, else the old children array will be modified
+        IncrementalParseForest[] parseForests =
+            ((IncrementalParseNode) currentForest).getFirstDerivation().parseForests().clone();
+        for(int i = 0; i < parseForests.length; i++) {
+            if(updates.isEmpty())
+                break;
+            // If the current subtree is after the previous to-be-deleted range, move to next update
+            if(currentOffset >= updates.getFirst().deletedEnd && currentOffset > 0)
+                updates.removeFirst();
+            if(updates.isEmpty())
+                break;
+
+            EditorUpdate update = updates.getFirst();
+            int deletedStartOffset = update.deletedStart;
+            int deletedEndOffset = update.deletedEnd;
+            String inserted = update.inserted;
+            // TODO store update type (insertion/deletion/replacement) in EditorUpdate
+            boolean isReplacement = inserted.length() > 0 && deletedStartOffset < deletedEndOffset;
+
+            IncrementalParseForest parseForest = parseForests[i];
+            int nextOffset = currentOffset + parseForest.width(); // == start offset of right sibling subtree
+
+            // Optimization: if current subtree starts exactly at deletedStart and it spans the subtree, replace it
+            if(deletedStartOffset == currentOffset && currentOffset < nextOffset && nextOffset <= deletedEndOffset
+                && isReplacement)
+                parseForests[i] = getParseNodeFromString(inserted);
+            // Optimization: if current subtree is a subrange within [deletedStart, deletedEnd], delete it
+            else if(deletedStartOffset <= currentOffset && nextOffset <= deletedEndOffset && isReplacement)
+                parseForests[i] = null;
+            // If current subtree (partially) overlaps with the to-be-deleted range, recurse
+            else if(deletedStartOffset <= nextOffset && currentOffset <= deletedEndOffset)
+                parseForests[i] = processUpdates(parseForest, currentOffset, updates);
+
+            currentOffset = nextOffset;
+        }
+        return newParseNodeFromChildren(parseForests);
+    }
+
+    private IncrementalParseNode newParseNodeFromChildren(IncrementalParseForest... newChildren) {
+        IncrementalParseForest[] filtered =
+            Arrays.stream(newChildren).filter(Objects::nonNull).toArray(IncrementalParseForest[]::new);
+        if(filtered.length == 0)
+            return null;
+        return parseForestManager.createChangedParseNode(incrementalParse, filtered);
+    }
+
+    public IncrementalParseNode getParseNodeFromString(String inputString) {
+        IncrementalParseForest[] parseForests = parseForestManager.parseForestsArray(inputString.length());
+
+        char[] chars = inputString.toCharArray();
+        for(int i = 0; i < chars.length; i++) {
+            parseForests[i] = parseForestManager.createCharacterNode(incrementalParse, chars[i]);
+        }
+        return parseForestManager.createChangedParseNode(incrementalParse, parseForests);
+    }
+}
---
+++ org.spoofax.jsglr2/src/test/java/org/spoofax/jsglr2/incremental/diff/ProcessUpdatesTest.java
@@ -0,0 +1,143 @@
+package org.spoofax.jsglr2.incremental.diff;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.spoofax.jsglr2.incremental.EditorUpdate;
+import org.spoofax.jsglr2.incremental.IncrementalParse;
+import org.spoofax.jsglr2.incremental.parseforest.IncrementalCharacterNode;
+import org.spoofax.jsglr2.incremental.parseforest.IncrementalParseForest;
+import org.spoofax.jsglr2.incremental.parseforest.IncrementalParseNode;
+import org.spoofax.jsglr2.parser.observing.ParserObserving;
+import org.spoofax.jsglr2.stack.collections.ActiveStacksFactory;
+import org.spoofax.jsglr2.stack.collections.ForActorStacksFactory;
+
+public class ProcessUpdatesTest {
+
+    private final ProcessUpdates<?> processUpdates = new ProcessUpdates<>(new IncrementalParse<>("", "",
+        new ActiveStacksFactory(), new ForActorStacksFactory(), new ParserObserving<>()));
+
+    @Test public void testDeleteSubtree() {
+        IncrementalParseNode previous = node(node(0, 1), node(2, 3), node(4, 5));
+
+        IncrementalParseNode expected = node(node(0, 1), node(4, 5));
+
+        testUpdate(previous, expected, new EditorUpdate(2, 4, ""));
+    }
+
+    @Test public void testPrepend() {
+        IncrementalParseNode previous = node(node(0), node(1, 2));
+
+        IncrementalParseNode expected = node(node(node(4, 5), node(0)), node(1, 2));
+
+        testUpdate(previous, expected, new EditorUpdate(0, 0, "\4\5"));
+    }
+
+    @Test public void testPrependBeforeEmpty() {
+        IncrementalParseNode previous = node(node(), node(0), node(1, 2));
+
+        IncrementalParseNode expected = node(node(node(4, 5), node(0)), node(1, 2));
+
+        testUpdate(previous, expected, new EditorUpdate(0, 0, "\4\5"));
+    }
+
+    @Test public void testAppend() {
+        IncrementalParseNode previous = node(node(0, 1), node(2, 3));
+
+        IncrementalParseNode expected = node(node(0, 1), node(node(2), node(node(3), node(4, 5))));
+
+        testUpdate(previous, expected, new EditorUpdate(4, 4, "\4\5"));
+    }
+
+    @Test public void testAppendBeforeEmpty() {
+        IncrementalParseNode previous = node(node(0, 1), node(2, 3), node());
+
+        IncrementalParseNode expected = node(node(0, 1), node(node(2), node(node(3), node(4, 5))), node());
+
+        testUpdate(previous, expected, new EditorUpdate(4, 4, "\4\5"));
+    }
+
+    @Test public void testInsert() {
+        IncrementalParseNode previous = node(node(0, 1), node(2, 3));
+
+        IncrementalParseNode expected = node(node(node(0), node(node(1), node(4, 5))), node(2, 3));
+
+        testUpdate(previous, expected, new EditorUpdate(2, 2, "\4\5"));
+    }
+
+    @Test public void testReplace() {
+        IncrementalParseNode previous = node(node(0, 1), node(2, 3));
+
+        IncrementalParseNode expected = node(node(node(0), node(4, 5)), node(2, 3));
+
+        testUpdate(previous, expected, new EditorUpdate(1, 2, "\4\5"));
+    }
+
+    @Test public void testReplaceFirst() {
+        IncrementalParseNode previous = node(node(0, 1), node(2, 3));
+
+        IncrementalParseNode expected = node(node(node(4, 5, 6), node(1)), node(2, 3));
+
+        testUpdate(previous, expected, new EditorUpdate(0, 1, "\4\5\6"));
+    }
+
+    @Test public void testReplaceFirstAfterEmpty() {
+        IncrementalParseNode previous = node(node(), node(0, 1), node(2, 3));
+
+        IncrementalParseNode expected = node(node(node(4, 5, 6), node(1)), node(2, 3));
+
+        testUpdate(previous, expected, new EditorUpdate(0, 1, "\4\5\6"));
+    }
+
+    @Test public void testReplaceEnd() {
+        IncrementalParseNode previous = node(node(node(0, 1), node(2)), node(3));
+
+        IncrementalParseNode expected = node(node(node(0, 1), node(4, 5, 6)));
+
+        testUpdate(previous, expected, new EditorUpdate(2, 4, "\4\5\6"));
+    }
+
+    @Test public void testReplaceMultipleWithGap() {
+        IncrementalParseNode previous = node(node(node(0, 1), node(2)), node(3));
+
+        IncrementalParseNode expected =
+            node(node(node(node(0), node(4, 5)), node(2)), node(6, 7));
+
+        testUpdate(previous, expected, new EditorUpdate(1, 2, "\4\5"), new EditorUpdate(3, 4, "\6\7"));
+    }
+
+    @Test public void testReplaceMultipleWithoutGap() {
+        IncrementalParseNode previous = node(node(node(0, 1), node(2)), node(3));
+
+        IncrementalParseNode expected =
+                node(node(node(node(0), node(4, 5)), node(6, 7)), node(3));
+
+        testUpdate(previous, expected, new EditorUpdate(1, 2, "\4\5"), new EditorUpdate(2, 3, "\6\7"));
+    }
+
+
+    private void testUpdate(IncrementalParseNode previous, IncrementalParseNode expected, EditorUpdate... updates) {
+        assertEquals(expected.toString(), processUpdates.processUpdates(previous, updates).toString());
+    }
+
+    private static IncrementalCharacterNode node(int i) {
+        return new IncrementalCharacterNode(i);
+    }
+
+    private static IncrementalParseNode node(int... characters) {
+        IncrementalParseForest[] children = new IncrementalParseForest[characters.length];
+        for(int i = 0; i < characters.length; i++) {
+            children[i] = node(characters[i]);
+        }
+        return new IncrementalParseNode(children);
+    }
+
+    private static IncrementalParseNode node(IncrementalParseForest... children) {
+        return new IncrementalParseNode(children);
+    }
+
+    private static IncrementalParseNode node() {
+        return new IncrementalParseNode();
+    }
+
+}

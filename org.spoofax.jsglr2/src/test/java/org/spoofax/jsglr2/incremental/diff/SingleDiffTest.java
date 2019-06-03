51094862fa30fb71c01359f4e6ca32b4fc0ef634
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2019-06-03 10:16:58 +0200

    Fix bug in SingleDiff regarding insertions at start

--- org.spoofax.jsglr2/src/test/java/org/spoofax/jsglr2/incremental/diff/SingleDiffTest.java
+++ org.spoofax.jsglr2/src/test/java/org/spoofax/jsglr2/incremental/diff/SingleDiffTest.java
@@ -13,7 +13,10 @@ public class SingleDiffTest {
         assertEquals(new EditorUpdate(2, 4, "fghij"), diff.diff("abcde", "abfghije").get(0));
         assertEquals(new EditorUpdate(5, 5, ""), diff.diff("abcde", "abcde").get(0));
         assertEquals(new EditorUpdate(0, 5, "fghij"), diff.diff("abcde", "fghij").get(0));
-        assertEquals(new EditorUpdate(1, 12, "uvw\nfghij\nxyz"),
+        assertEquals(new EditorUpdate(0, 0, "xyz"), diff.diff("abcde", "xyzabcde").get(0));
+        assertEquals(new EditorUpdate(5, 5, "xyz"), diff.diff("abcde", "abcdexyz").get(0));
+        assertEquals(new EditorUpdate(3, 4, "*"), diff.diff("x+x+x", "x+x*x").get(0));
+        assertEquals(new EditorUpdate(1, 12, "uvwde\nfghij\nxyz"),
             diff.diff("abcde\nfghij\nklmno", "auvwde\nfghij\nxyzklmno").get(0));
     }
 

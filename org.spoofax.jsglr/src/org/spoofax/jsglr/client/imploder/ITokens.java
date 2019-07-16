68e9dcd172060235fcb2f6dca17e40dc203eaf23
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2019-07-16 14:55:40 +0200

    Remove redundant access modifiers in interfaces

--- org.spoofax.jsglr/src/org/spoofax/jsglr/client/imploder/ITokens.java
+++ org.spoofax.jsglr/src/org/spoofax/jsglr/client/imploder/ITokens.java
@@ -3,22 +3,22 @@ package org.spoofax.jsglr.client.imploder;
 import java.io.Serializable;
 
 public interface ITokens extends Iterable<IToken>, Serializable {
-    
-    public static final String ERROR_SKIPPED_REGION = "Syntax error, unexpected construct(s)";
-    
-    public static final String ERROR_UNEXPECTED_EOF = "Syntax error, unexpected end of input";
-    
-    public static final String ERROR_WATER_PREFIX = "Syntax error, not expected here";
-    
-    public static final String ERROR_INSERT_PREFIX = "Syntax error, expected";
-    
-    public static final String ERROR_INSERT_END_PREFIX = "Syntax error, unterminated construct";
 
-    public static final String ERROR_INCOMPLETE_PREFIX = "Syntax error, incomplete construct";
+    String ERROR_SKIPPED_REGION = "Syntax error, unexpected construct(s)";
 
-    public static final String ERROR_GENERIC_PREFIX = "Syntax error";
+    String ERROR_UNEXPECTED_EOF = "Syntax error, unexpected end of input";
 
-    public static final String ERROR_WARNING_PREFIX = "Warning";
+    String ERROR_WATER_PREFIX = "Syntax error, not expected here";
+
+    String ERROR_INSERT_PREFIX = "Syntax error, expected";
+
+    String ERROR_INSERT_END_PREFIX = "Syntax error, unterminated construct";
+
+    String ERROR_INCOMPLETE_PREFIX = "Syntax error, incomplete construct";
+
+    String ERROR_GENERIC_PREFIX = "Syntax error";
+
+    String ERROR_WARNING_PREFIX = "Warning";
 
     String getInput();
 
@@ -33,14 +33,13 @@ public interface ITokens extends Iterable<IToken>, Serializable {
     String toString(IToken left, IToken right);
 
     String toString(int startOffset, int endOffset);
-    
+
     /**
-     * Determines if the tokenizer is ambiguous.
-     * If it is, tokens with subsequent indices may not
-     * always have matching start/end offsets.
+     * Determines if the tokenizer is ambiguous. If it is, tokens with subsequent indices may not always have matching
+     * start/end offsets.
      * 
-     * @see Tokenizer#getTokenAfter(IToken)   Gets the next token with a matching offset.
-     * @see Tokenizer#getTokenBefore(IToken)  Gets the previous token with a matching offset.
+     * @see Tokenizer#getTokenAfter(IToken) Gets the next token with a matching offset.
+     * @see Tokenizer#getTokenBefore(IToken) Gets the previous token with a matching offset.
      */
     boolean isAmbiguous();
 

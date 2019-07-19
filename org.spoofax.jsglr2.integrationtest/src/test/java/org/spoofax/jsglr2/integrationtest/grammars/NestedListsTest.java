a358b83a747a0236a7f3ccc8c472eab4e1e50d26
Jasper Denkers <j.denkers@tudelft.nl>
2019-07-19 15:20:40 +0200

    Add test cases and problem description for nested lists (ignore failing test for now)

---
+++ org.spoofax.jsglr2.integrationtest/src/test/java/org/spoofax/jsglr2/integrationtest/grammars/NestedListsTest.java
@@ -0,0 +1,54 @@
+package org.spoofax.jsglr2.integrationtest.grammars;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.spoofax.jsglr2.integrationtest.BaseTestWithSdf3ParseTables;
+import org.spoofax.terms.ParseError;
+
+public class NestedListsTest extends BaseTestWithSdf3ParseTables {
+
+    public NestedListsTest() {
+        super("nestedlists.sdf3");
+    }
+
+    @Test public void testX() throws ParseError {
+        testSuccessByExpansions("x", "X()");
+    }
+
+    @Test public void testMX() throws ParseError {
+        testSuccessByExpansions("lx", "L([X()])");
+    }
+
+    @Test public void testMXX() throws ParseError {
+        testSuccessByExpansions("lxx", "L([X(),X()])");
+    }
+
+    @Test public void testMMXX() throws ParseError {
+        String list1 = "[L([X()]),X()]"; // One in inner list, one in outer list
+        String list2 = "[L([X(),X()])]"; // Both in inner list
+
+        testSuccessByExpansions("llxx", "L(amb([" + list1 + "," + list2 + "]))");
+    }
+
+    /*
+     * testMMXXX1 fails and testMMXXX2 succeeds. However, it seems like it should be the other way around, i.e., no
+     * extra wrapping by a list term.
+     */
+
+    @Ignore @Test public void testMMXXX1() throws ParseError {
+        String list1 = "[L([X()]),X(),X()]"; // One in inner list, two in outer list
+        String list2 = "[L([X(),X()]),X()]"; // Two in inner list, one in outer list
+        String list3 = "[L([X(),X(),X()])]"; // All in inner list
+
+        testSuccessByExpansions("llxxx", "L(amb([" + list1 + "," + list2 + "," + list3 + "]))");
+    }
+
+    @Test public void testMMXXX2() throws ParseError {
+        String list1 = "[[L([X()]),X()],X()]"; // One in inner list, two in outer list
+        String list2 = "[[L([X(),X()])],X()]"; // Two in inner list, one in outer list
+        String list3 = "[L([X(),X(),X()])]"; // All in inner list
+
+        testSuccessByExpansions("llxxx", "L(amb([" + list1 + "," + list2 + "," + list3 + "]))");
+    }
+
+}

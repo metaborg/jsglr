6501ca4d04b21bda0f9e0d9abef04960447ee645
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2019-08-16 13:45:56 +0200

    Factor out common superclass for incremental benchmarks
    With or without imploding is almost the same, so most of the
    functionality has been pulled up.

---
+++ org.spoofax.jsglr2.benchmark/src/main/java/org/spoofax/jsglr2/benchmark/jsglr2/JSGLR2BenchmarkIncremental.java
@@ -0,0 +1,122 @@
+package org.spoofax.jsglr2.benchmark.jsglr2;
+
+import java.util.*;
+
+import org.metaborg.parsetable.query.ActionsForCharacterRepresentation;
+import org.metaborg.parsetable.query.ProductionToGotoRepresentation;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Setup;
+import org.spoofax.interpreter.terms.IStrategoTerm;
+import org.spoofax.jsglr2.JSGLR2Variant;
+import org.spoofax.jsglr2.imploder.TreeImploder;
+import org.spoofax.jsglr2.imploder.incremental.IncrementalImplodeInput;
+import org.spoofax.jsglr2.imploder.incremental.IncrementalTreeImploder;
+import org.spoofax.jsglr2.incremental.IncrementalParser;
+import org.spoofax.jsglr2.incremental.parseforest.IncrementalParseForest;
+import org.spoofax.jsglr2.integration.IntegrationVariant;
+import org.spoofax.jsglr2.integration.ParseTableVariant;
+import org.spoofax.jsglr2.parseforest.IDerivation;
+import org.spoofax.jsglr2.parseforest.IParseForest;
+import org.spoofax.jsglr2.parseforest.IParseNode;
+import org.spoofax.jsglr2.parser.ParseException;
+import org.spoofax.jsglr2.testset.TestSetReader;
+import org.spoofax.jsglr2.testset.testinput.IncrementalStringInput;
+
+public abstract class JSGLR2BenchmarkIncremental extends JSGLR2Benchmark<String[], IncrementalStringInput> {
+
+    public JSGLR2BenchmarkIncremental(TestSetReader<String[], IncrementalStringInput> testSetReader) {
+        super(testSetReader);
+    }
+
+    public enum ParserType {
+        Batch(false,
+            new IntegrationVariant(new ParseTableVariant(ActionsForCharacterRepresentation.DisjointSorted,
+                ProductionToGotoRepresentation.JavaHashMap), JSGLR2Variant.Preset.standard.variant)),
+        Incremental(true,
+            new IntegrationVariant(new ParseTableVariant(ActionsForCharacterRepresentation.DisjointSorted,
+                ProductionToGotoRepresentation.JavaHashMap), JSGLR2Variant.Preset.incremental.variant)),
+        IncrementalNoCache(false,
+            new IntegrationVariant(new ParseTableVariant(ActionsForCharacterRepresentation.DisjointSorted,
+                ProductionToGotoRepresentation.JavaHashMap), JSGLR2Variant.Preset.incremental.variant));
+
+        boolean setupCache;
+        IntegrationVariant integrationVariant;
+
+        ParserType(boolean setupCache, IntegrationVariant integrationVariant) {
+            this.setupCache = setupCache;
+            this.integrationVariant = integrationVariant;
+        }
+    }
+
+    @Param({ "Batch", "Incremental", "IncrementalNoCache" }) public ParserType parserType;
+
+    @Param({ "-1" }) public int i;
+
+    @Override protected IntegrationVariant variant() {
+        return parserType.integrationVariant;
+    }
+
+    Map<IncrementalStringInput, String> prevString = new HashMap<>();
+    Map<IncrementalStringInput, IncrementalParseForest> prevResult = new HashMap<>();
+    Map<IncrementalStringInput, WeakHashMap<IParseNode<IParseForest, IDerivation<IParseForest>>, TreeImploder.SubTree<IStrategoTerm>>> prevMap =
+        new HashMap<>();
+
+    Map<IncrementalStringInput, List<String>> uniqueInputs = new HashMap<>();
+
+    protected boolean shouldSetupCache() {
+        return parserType.setupCache && i > 0;
+    }
+
+    @Setup public void setupCache() throws ParseException {
+        if(i == -2) {
+            for(IncrementalStringInput input : inputs) {
+                List<String> res = new ArrayList<>();
+                String prev = null;
+                for(String s : input.content) {
+                    if(s.length() == 0)
+                        continue;
+                    if(!s.equals(prev)) {
+                        res.add(s);
+                        prev = s;
+                    }
+                }
+                uniqueInputs.put(input, res);
+            }
+        }
+        if(shouldSetupCache()) {
+            for(IncrementalStringInput input : inputs) {
+                String content = input.content[i - 1];
+                prevString.put(input, content);
+                prevResult.put(input,
+                    ((IncrementalParseForest) jsglr2.parser.parseUnsafe(content, input.filename, null)));
+                if(implode()) {
+                    IncrementalTreeImploder<IParseForest, IParseNode<IParseForest, IDerivation<IParseForest>>, IDerivation<IParseForest>, IStrategoTerm, IncrementalImplodeInput<IParseNode<IParseForest, IDerivation<IParseForest>>, IStrategoTerm>> imploder =
+                        (IncrementalTreeImploder) jsglr2.imploder;
+                    imploder.implode(prevString.get(input), input.filename, prevResult.get(input));
+                    prevMap.put(input, imploder.getFromCache(input.filename));
+                }
+            }
+        }
+    }
+
+    protected void correctCache(IncrementalStringInput input) {
+        if(jsglr2.parser instanceof IncrementalParser) {
+            IncrementalParser parser = (IncrementalParser) jsglr2.parser;
+            parser.clearCache();
+            if(shouldSetupCache())
+                parser.addToCache(input.filename, prevString.get(input), prevResult.get(input));
+        }
+        if(implode() && jsglr2.imploder instanceof IncrementalTreeImploder) {
+            IncrementalTreeImploder imploder = (IncrementalTreeImploder) jsglr2.imploder;
+            imploder.clearCache();
+            if(shouldSetupCache())
+                imploder.addToCache(input.filename, new WeakHashMap<>(prevMap.get(input)));
+        }
+    }
+
+    // Only used in ManualBenchmark
+    Iterable<IncrementalStringInput> getInputs() {
+        return inputs;
+    }
+
+}
--- org.spoofax.jsglr2.benchmark/src/main/java/org/spoofax/jsglr2/benchmark/jsglr2/JSGLR2BenchmarkIncrementalParsing.java
+++ org.spoofax.jsglr2.benchmark/src/main/java/org/spoofax/jsglr2/benchmark/jsglr2/JSGLR2BenchmarkIncrementalParsing.java
@@ -1,115 +1,23 @@
 package org.spoofax.jsglr2.benchmark.jsglr2;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.metaborg.parsetable.query.ActionsForCharacterRepresentation;
-import org.metaborg.parsetable.query.ProductionToGotoRepresentation;
-import org.openjdk.jmh.annotations.Param;
-import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.infra.Blackhole;
-import org.spoofax.jsglr2.JSGLR2Variants.ParserVariant;
 import org.spoofax.jsglr2.benchmark.BenchmarkTestSetReader;
-import org.spoofax.jsglr2.imploder.ImploderVariant;
-import org.spoofax.jsglr2.incremental.IncrementalParser;
-import org.spoofax.jsglr2.incremental.parseforest.IncrementalParseForest;
-import org.spoofax.jsglr2.integration.IntegrationVariant;
-import org.spoofax.jsglr2.integration.ParseTableVariant;
-import org.spoofax.jsglr2.parseforest.ParseForestConstruction;
-import org.spoofax.jsglr2.parseforest.ParseForestRepresentation;
 import org.spoofax.jsglr2.parser.ParseException;
-import org.spoofax.jsglr2.reducing.Reducing;
-import org.spoofax.jsglr2.stack.StackRepresentation;
-import org.spoofax.jsglr2.stack.collections.ActiveStacksRepresentation;
-import org.spoofax.jsglr2.stack.collections.ForActorStacksRepresentation;
 import org.spoofax.jsglr2.testset.TestSet;
 import org.spoofax.jsglr2.testset.testinput.IncrementalStringInput;
-import org.spoofax.jsglr2.tokens.TokenizerVariant;
 
-public abstract class JSGLR2BenchmarkIncrementalParsing extends JSGLR2Benchmark<String[], IncrementalStringInput> {
+public abstract class JSGLR2BenchmarkIncrementalParsing extends JSGLR2BenchmarkIncremental {
 
     protected JSGLR2BenchmarkIncrementalParsing(TestSet<String[], IncrementalStringInput> testSet) {
         super(new BenchmarkTestSetReader<>(testSet));
     }
 
-    @Param({ "false" }) public boolean implode;
-
-    @Param({ "DisjointSorted" }) ActionsForCharacterRepresentation actionsForCharacterRepresentation;
-
-    @Param({ "JavaHashMap" }) ProductionToGotoRepresentation productionToGotoRepresentation;
-
-    @Param({ "ArrayList" }) public ActiveStacksRepresentation activeStacksRepresentation;
-
-    @Param({ "ArrayDeque" }) public ForActorStacksRepresentation forActorStacksRepresentation;
-
-    @Param({ "Hybrid", "Incremental" }) public ParseForestRepresentation parseForestRepresentation;
-
-    @Param({ "Full" }) public ParseForestConstruction parseForestConstruction;
-
-    @Param({ "Hybrid" }) public StackRepresentation stackRepresentation;
-
-    @Param({ "Basic" }) public Reducing reducing;
-
-    @Param({ "TokenizedRecursive" }) public ImploderVariant imploder;
-
-    @Param({ "-1" }) public int i;
-
-    Map<IncrementalStringInput, String> prevString = new HashMap<>();
-    Map<IncrementalStringInput, IncrementalParseForest> prevResult = new HashMap<>();
-    Map<IncrementalStringInput, List<String>> uniqueInputs = new HashMap<>();
-
-    @Setup public void setupCache() throws ParseException {
-        if(i == -2) {
-            for(IncrementalStringInput input : inputs) {
-                List<String> res = new ArrayList<>();
-                String prev = null;
-                for(String s : input.content) {
-                    if(s.length() == 0)
-                        continue;
-                    if(!s.equals(prev)) {
-                        res.add(s);
-                        prev = s;
-                    }
-                }
-                uniqueInputs.put(input, res);
-            }
-        }
-        if(i > 0) {
-            for(IncrementalStringInput input : inputs) {
-                if(jsglr2.parser instanceof IncrementalParser) {
-                    String content = input.content[i - 1];
-                    prevString.put(input, content);
-                    prevResult.put(input,
-                        ((IncrementalParseForest) jsglr2.parser.parseUnsafe(content, input.filename, null)));
-                }
-            }
-        }
-    }
-
-    @Override protected IntegrationVariant variant() {
-        if(implode)
-            throw new IllegalStateException("this variant is not used for benchmarking");
-
-        return new IntegrationVariant(
-            new ParseTableVariant(actionsForCharacterRepresentation, productionToGotoRepresentation),
-            new ParserVariant(activeStacksRepresentation, forActorStacksRepresentation, parseForestRepresentation,
-                parseForestConstruction, stackRepresentation, reducing),
-            imploder, TokenizerVariant.Null);
-    }
-
     @Override protected boolean implode() {
-        return implode;
+        return false;
     }
 
     @Override protected Object action(Blackhole bh, IncrementalStringInput input) throws ParseException {
-        if(jsglr2.parser instanceof IncrementalParser) {
-            IncrementalParser parser = (IncrementalParser) jsglr2.parser;
-            parser.clearCache();
-            if(i > 0)
-                parser.addToCache(input.filename, prevString.get(input), prevResult.get(input));
-        }
+        correctCache(input);
 
         if(i >= 0)
             return jsglr2.parser.parseUnsafe(input.content[i], input.filename, null);
@@ -121,6 +29,7 @@ public abstract class JSGLR2BenchmarkIncrementalParsing extends JSGLR2Benchmark<
             return null;
         }
 
+        // if (i == -1)
         for(String content : input.content) {
             bh.consume(jsglr2.parser.parseUnsafe(content, input.filename, null));
         }

da17f9bb23270fcc353bab3d9f3920fae2d12146
Jasper Denkers <j.denkers@tudelft.nl>
2019-09-20 12:48:51 +0200

    Remove unnecessary layer of type parameters for layout-sensitive

--- org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/layoutsensitive/LayoutSensitiveReduceManager.java
+++ org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/layoutsensitive/LayoutSensitiveReduceManager.java
@@ -4,8 +4,6 @@ import org.metaborg.parsetable.IParseTable;
 import org.metaborg.parsetable.actions.IReduce;
 import org.metaborg.sdf2table.grammar.LayoutConstraintAttribute;
 import org.metaborg.sdf2table.parsetable.ParseTableProduction;
-import org.spoofax.jsglr2.parseforest.IDerivation;
-import org.spoofax.jsglr2.parseforest.IParseNode;
 import org.spoofax.jsglr2.parseforest.ParseForestConstruction;
 import org.spoofax.jsglr2.parseforest.ParseForestManager;
 import org.spoofax.jsglr2.parser.AbstractParseState;
@@ -20,44 +18,40 @@ import org.spoofax.jsglr2.stack.paths.StackPath;
 
 public class LayoutSensitiveReduceManager
 //@formatter:off
-   <ParseForest extends ILayoutSensitiveParseForest,
-    Derivation  extends IDerivation<ParseForest>,
-    ParseNode   extends IParseNode<ParseForest, Derivation>,
-    StackNode   extends IStackNode,
-    ParseState  extends AbstractParseState<ParseForest, StackNode>>
+   <StackNode   extends IStackNode,
+    ParseState  extends AbstractParseState<ILayoutSensitiveParseForest, StackNode>>
 //@formatter:on
-    extends ReduceManager<ParseForest, Derivation, ParseNode, StackNode, ParseState> {
+    extends
+    ReduceManager<ILayoutSensitiveParseForest, ILayoutSensitiveDerivation<ILayoutSensitiveParseForest>, ILayoutSensitiveParseNode<ILayoutSensitiveParseForest, ILayoutSensitiveDerivation<ILayoutSensitiveParseForest>>, StackNode, ParseState> {
 
     public LayoutSensitiveReduceManager(IParseTable parseTable,
-        AbstractStackManager<ParseForest, Derivation, ParseNode, StackNode, ParseState> stackManager,
-        ParseForestManager<ParseForest, Derivation, ParseNode, StackNode, ParseState> parseForestManager,
+        AbstractStackManager<ILayoutSensitiveParseForest, ILayoutSensitiveDerivation<ILayoutSensitiveParseForest>, ILayoutSensitiveParseNode<ILayoutSensitiveParseForest, ILayoutSensitiveDerivation<ILayoutSensitiveParseForest>>, StackNode, ParseState> stackManager,
+        ParseForestManager<ILayoutSensitiveParseForest, ILayoutSensitiveDerivation<ILayoutSensitiveParseForest>, ILayoutSensitiveParseNode<ILayoutSensitiveParseForest, ILayoutSensitiveDerivation<ILayoutSensitiveParseForest>>, StackNode, ParseState> parseForestManager,
         ParseForestConstruction parseForestConstruction) {
         super(parseTable, stackManager, parseForestManager, parseForestConstruction);
     }
 
     public static
     //@formatter:off
-       <ParseForest_  extends ILayoutSensitiveParseForest,
-        Derivation_   extends IDerivation<ParseForest_>,
-        ParseNode_    extends IParseNode<ParseForest_, Derivation_>,
-        StackNode_    extends IStackNode,
-        ParseState_   extends AbstractParseState<ParseForest_, StackNode_>,
-        StackManager_ extends AbstractStackManager<ParseForest_, Derivation_, ParseNode_, StackNode_, ParseState_>>
+       <StackNode_    extends IStackNode,
+        ParseState_   extends AbstractParseState<ILayoutSensitiveParseForest, StackNode_>,
+        StackManager_ extends AbstractStackManager<ILayoutSensitiveParseForest, ILayoutSensitiveDerivation<ILayoutSensitiveParseForest>, ILayoutSensitiveParseNode<ILayoutSensitiveParseForest, ILayoutSensitiveDerivation<ILayoutSensitiveParseForest>>, StackNode_, ParseState_>>
     //@formatter:on
-    ReduceManagerFactory<ParseForest_, Derivation_, ParseNode_, StackNode_, ParseState_, StackManager_, LayoutSensitiveReduceManager<ParseForest_, Derivation_, ParseNode_, StackNode_, ParseState_>>
+    ReduceManagerFactory<ILayoutSensitiveParseForest, ILayoutSensitiveDerivation<ILayoutSensitiveParseForest>, ILayoutSensitiveParseNode<ILayoutSensitiveParseForest, ILayoutSensitiveDerivation<ILayoutSensitiveParseForest>>, StackNode_, ParseState_, StackManager_, LayoutSensitiveReduceManager<StackNode_, ParseState_>>
         factoryLayoutSensitive(ParserVariant parserVariant) {
         return (parseTable, stackManager, parseForestManager) -> new LayoutSensitiveReduceManager<>(parseTable,
             stackManager, parseForestManager, parserVariant.parseForestConstruction);
     }
 
     @Override protected void doReductionsHelper(
-        ParserObserving<ParseForest, Derivation, ParseNode, StackNode, ParseState> observing, ParseState parseState,
-        StackNode stack, IReduce reduce, StackLink<ParseForest, StackNode> throughLink) {
-        pathsLoop: for(StackPath<ParseForest, StackNode> path : stackManager.findAllPathsOfLength(stack,
+        ParserObserving<ILayoutSensitiveParseForest, ILayoutSensitiveDerivation<ILayoutSensitiveParseForest>, ILayoutSensitiveParseNode<ILayoutSensitiveParseForest, ILayoutSensitiveDerivation<ILayoutSensitiveParseForest>>, StackNode, ParseState> observing,
+        ParseState parseState, StackNode stack, IReduce reduce,
+        StackLink<ILayoutSensitiveParseForest, StackNode> throughLink) {
+        pathsLoop: for(StackPath<ILayoutSensitiveParseForest, StackNode> path : stackManager.findAllPathsOfLength(stack,
             reduce.arity())) {
             if(throughLink == null || path.contains(throughLink)) {
                 StackNode pathBegin = path.head();
-                ParseForest[] parseNodes = stackManager.getParseForests(parseForestManager, path);
+                ILayoutSensitiveParseForest[] parseNodes = stackManager.getParseForests(parseForestManager, path);
 
                 if(reduce.production() instanceof ParseTableProduction) {
                     ParseTableProduction sdf2tableProduction = (ParseTableProduction) reduce.production();

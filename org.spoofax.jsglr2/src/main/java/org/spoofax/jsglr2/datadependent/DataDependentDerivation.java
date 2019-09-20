ff277957ab6e9e13bcb69baf0638e870f507fc89
Jasper Denkers <j.denkers@tudelft.nl>
2019-09-20 10:03:12 +0200

    Factor out calculating of context bitmaps to enable reuse in composite parser

--- org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/datadependent/DataDependentDerivation.java
+++ org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/datadependent/DataDependentDerivation.java
@@ -11,16 +11,27 @@ public class DataDependentDerivation
 //@formatter:on
     extends BasicDerivation<ParseForest> implements IDataDependentDerivation<ParseForest> {
 
-    private long contextBitmap = 0L;
+    private final long contextBitmap;
 
     public DataDependentDerivation(IProduction production, ProductionType productionType, ParseForest[] parseForests) {
         super(production, productionType, parseForests);
 
+        contextBitmap = calculateContextBitmap(parseForests);
+    }
+
+    public final long getContextBitmap() {
+        return contextBitmap;
+    }
+
+    static private <ParseForest_ extends IDataDependentParseForest> long
+        calculateContextBitmap(ParseForest_[] parseForests) {
+        long contextBitmap = 0L;
+
         if(parseForests.length > 0) {
             if(parseForests.length == 1) {
                 if(parseForests[0] instanceof IDataDependentParseNode) {
-                    final IDataDependentParseNode<ParseForest, IDataDependentDerivation<ParseForest>> parseNode =
-                        (IDataDependentParseNode<ParseForest, IDataDependentDerivation<ParseForest>>) parseForests[0];
+                    final IDataDependentParseNode<ParseForest_, IDataDependentDerivation<ParseForest_>> parseNode =
+                        (IDataDependentParseNode<ParseForest_, IDataDependentDerivation<ParseForest_>>) parseForests[0];
                     final ParseTableProduction onlyProduction = (ParseTableProduction) parseNode.production();
 
                     // introduction of contextual token
@@ -28,28 +39,28 @@ public class DataDependentDerivation
                     contextBitmap |= onlyProduction.contextR();
 
                     // aggregation of recursive contextual tokens
-                    for(IDataDependentDerivation<ParseForest> derivation : parseNode.getDerivations()) {
+                    for(IDataDependentDerivation<ParseForest_> derivation : parseNode.getDerivations()) {
                         contextBitmap |= (derivation.getContextBitmap());
                     }
                 }
             } else {
                 if(parseForests[0] instanceof IDataDependentParseNode) {
-                    final IDataDependentParseNode<ParseForest, IDataDependentDerivation<ParseForest>> parseForest =
-                        (IDataDependentParseNode<ParseForest, IDataDependentDerivation<ParseForest>>) parseForests[0];
+                    final IDataDependentParseNode<ParseForest_, IDataDependentDerivation<ParseForest_>> parseForest =
+                        (IDataDependentParseNode<ParseForest_, IDataDependentDerivation<ParseForest_>>) parseForests[0];
                     final ParseTableProduction leftmostProduction = (ParseTableProduction) parseForest.production();
 
                     // introduction of contextual token
                     contextBitmap |= leftmostProduction.contextL();
 
                     // aggregation of recursive contextual tokens
-                    for(IDataDependentDerivation<ParseForest> derivation : parseForest.getDerivations()) {
+                    for(IDataDependentDerivation<ParseForest_> derivation : parseForest.getDerivations()) {
                         contextBitmap |= (derivation.getContextBitmap());
                     }
                 }
 
                 if(parseForests[parseForests.length - 1] instanceof IDataDependentParseNode) {
-                    final IDataDependentParseNode<ParseForest, IDataDependentDerivation<ParseForest>> parseForest =
-                        (IDataDependentParseNode<ParseForest, IDataDependentDerivation<ParseForest>>) parseForests[parseForests.length
+                    final IDataDependentParseNode<ParseForest_, IDataDependentDerivation<ParseForest_>> parseForest =
+                        (IDataDependentParseNode<ParseForest_, IDataDependentDerivation<ParseForest_>>) parseForests[parseForests.length
                             - 1];
                     final ParseTableProduction rightmostProduction = (ParseTableProduction) parseForest.production();
 
@@ -57,16 +68,14 @@ public class DataDependentDerivation
                     contextBitmap |= rightmostProduction.contextR();
 
                     // aggregation of recursive contextual tokens
-                    for(IDataDependentDerivation<ParseForest> derivation : parseForest.getDerivations()) {
+                    for(IDataDependentDerivation<ParseForest_> derivation : parseForest.getDerivations()) {
                         contextBitmap |= (derivation.getContextBitmap());
                     }
                 }
 
             }
         }
-    }
 
-    public final long getContextBitmap() {
         return contextBitmap;
     }
 

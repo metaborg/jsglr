1f65f609fcb72cdb6835fd6811e16f8d454910a1
Jasper Denkers <j.denkers@tudelft.nl>
2019-09-24 15:45:37 +0200

    Move longest match filtering in layout-sensitive parse node to interface, only fields in implementation

--- org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/layoutsensitive/ILayoutSensitiveParseNode.java
+++ org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/layoutsensitive/ILayoutSensitiveParseNode.java
@@ -1,5 +1,6 @@
 package org.spoofax.jsglr2.layoutsensitive;
 
+import com.google.common.collect.Lists;
 import org.spoofax.jsglr2.parseforest.basic.IBasicParseNode;
 
 import java.util.List;
@@ -11,8 +12,87 @@ public interface ILayoutSensitiveParseNode
 //@formatter:on
     extends IBasicParseNode<ParseForest, Derivation>, ILayoutSensitiveParseForest {
 
-    void filterLongestMatchDerivations();
+    boolean filteredLongestMatch();
 
-    List<PositionInterval> getLongestMatchPositions();
+    void setFilteredLongestMatch();
+
+    default void filterLongestMatchDerivations() {
+        setFilteredLongestMatch();
+
+        if(getDerivations().size() <= 1) {
+            setLongestMatchPositions(getFirstDerivation().getLongestMatchPositions());
+
+            return;
+        }
+
+        List<List<PositionInterval>> longestMatchNodes = Lists.newArrayList();
+
+        for(Derivation derivation : getDerivations()) {
+            longestMatchNodes.add(derivation.getLongestMatchPositions());
+        }
+
+        int size = -1;
+        int currentLongestDerivation = 0;
+        boolean disambiguatedLongestMatch = false;
+
+        for(int i = 1; i < longestMatchNodes.size(); i++) {
+            List<PositionInterval> list = longestMatchNodes.get(i);
+
+            // FIXME: list of longest-match nodes should be the same?
+            if(size == -1) {
+                size = list.size();
+            } else if(size != list.size()) {
+                System.out.println("Number of longest match nodes differ");
+            }
+
+            for(int j = 0; j < list.size(); j++) {
+                Boolean secondNodeExpandsLonger =
+                    expandsLonger(longestMatchNodes.get(currentLongestDerivation).get(j), list.get(j));
+                if(secondNodeExpandsLonger == null) {
+                    continue;
+                } else if(secondNodeExpandsLonger) {
+                    currentLongestDerivation = i;
+                    disambiguatedLongestMatch = true;
+                    break;
+                } else {
+                    disambiguatedLongestMatch = true;
+                    break;
+                }
+            }
+        }
+
+        if(disambiguatedLongestMatch) {
+            Derivation longestDerivation = getDerivations().get(currentLongestDerivation);
+            getDerivations().clear();
+            getDerivations().add(longestDerivation);
+        }
+
+        setLongestMatchPositions(longestMatchNodes.get(currentLongestDerivation));
+    }
+
+    default Boolean expandsLonger(PositionInterval pos1, PositionInterval pos2) {
+        assert (pos1.getStart().equals(pos2.getStart()));
+
+        if(pos2.getEnd().line > pos1.getEnd().line
+            || (pos2.getEnd().line == pos1.getEnd().line && pos2.getEnd().column > pos1.getEnd().column)) {
+            return true;
+        } else if(pos1.getEnd().line > pos2.getEnd().line
+            || (pos1.getEnd().line == pos2.getEnd().line && pos1.getEnd().column > pos2.getEnd().column)) {
+            return false;
+        }
+
+        return null;
+    }
+
+    void setLongestMatchPositions(List<PositionInterval> longestMatchPositions);
+
+    List<PositionInterval> longestMatchPositions();
+
+    default List<PositionInterval> getLongestMatchPositions() {
+        if(longestMatchPositions() == null && !filteredLongestMatch())
+            filterLongestMatchDerivations();
+
+        return longestMatchPositions();
+    }
 
 }
--- org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/layoutsensitive/LayoutSensitiveParseNode.java
+++ org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/layoutsensitive/LayoutSensitiveParseNode.java
@@ -1,6 +1,5 @@
 package org.spoofax.jsglr2.layoutsensitive;
 
-import com.google.common.collect.Lists;
 import org.metaborg.parsetable.productions.IProduction;
 import org.spoofax.jsglr2.parseforest.basic.BasicParseNode;
 import org.spoofax.jsglr2.parser.Position;
@@ -16,7 +15,7 @@ class LayoutSensitiveParseNode
 
     private final Position startPosition, endPosition;
 
-    private List<PositionInterval> longestMatchPos = null;
+    private List<PositionInterval> longestMatchPositions = null;
     private boolean filteredLongestMatch = false;
 
     LayoutSensitiveParseNode(Position startPosition, Position endPosition, IProduction production) {
@@ -34,80 +33,20 @@ class LayoutSensitiveParseNode
         return endPosition;
     }
 
-    @Override public void filterLongestMatchDerivations() {
+    @Override public boolean filteredLongestMatch() {
+        return filteredLongestMatch;
+    }
+
+    @Override public void setFilteredLongestMatch() {
         filteredLongestMatch = true;
-        longestMatchPos = getFirstDerivation().getLongestMatchPositions();
-        if(derivations.size() <= 1) {
-            return;
-        }
-
-        List<List<PositionInterval>> longestMatchNodes = Lists.newArrayList();
-
-        for(Derivation derivation : derivations) {
-            longestMatchNodes.add(derivation.getLongestMatchPositions());
-        }
-
-        int size = -1;
-        int currentLongestDerivation = 0;
-        boolean disambiguatedLongestMatch = false;
-
-        for(int i = 1; i < longestMatchNodes.size(); i++) {
-            List<PositionInterval> list = longestMatchNodes.get(i);
-
-            // FIXME: list of longest-match nodes should be the same?
-            if(size == -1) {
-                size = list.size();
-            } else if(size != list.size()) {
-                System.out.println("Number of longest match nodes differ");
-            }
-
-            for(int j = 0; j < list.size(); j++) {
-                Boolean secondNodeExpandsLonger =
-                    expandsLonger(longestMatchNodes.get(currentLongestDerivation).get(j), list.get(j));
-                if(secondNodeExpandsLonger == null) {
-                    continue;
-                } else if(secondNodeExpandsLonger) {
-                    currentLongestDerivation = i;
-                    disambiguatedLongestMatch = true;
-                    break;
-                } else {
-                    disambiguatedLongestMatch = true;
-                    break;
-                }
-            }
-        }
-
-        if(disambiguatedLongestMatch) {
-            Derivation longestDerivation = derivations.get(currentLongestDerivation);
-            derivations.clear();
-            derivations.add(longestDerivation);
-        }
-
-        longestMatchPos = longestMatchNodes.get(currentLongestDerivation);
     }
 
-    private Boolean expandsLonger(PositionInterval pos1, PositionInterval pos2) {
-        assert (pos1.getStart().equals(pos2.getStart()));
-
-        if(pos2.getEnd().line > pos1.getEnd().line
-            || (pos2.getEnd().line == pos1.getEnd().line && pos2.getEnd().column > pos1.getEnd().column)) {
-            return true;
-        } else if(pos1.getEnd().line > pos2.getEnd().line
-            || (pos1.getEnd().line == pos2.getEnd().line && pos1.getEnd().column > pos2.getEnd().column)) {
-            return false;
-        }
-
-        return null;
+    @Override public List<PositionInterval> longestMatchPositions() {
+        return longestMatchPositions;
     }
 
-    @Override public List<PositionInterval> getLongestMatchPositions() {
-        if(longestMatchPos == null && !filteredLongestMatch) {
-            filterLongestMatchDerivations();
-
-            return longestMatchPos;
-        } else {
-            return longestMatchPos;
-        }
+    @Override public void setLongestMatchPositions(List<PositionInterval> longestMatchPositions) {
+        this.longestMatchPositions = longestMatchPositions;
     }
 
 }

040bf310d59ab6e1eea72be5d57928957d36e82d
Jasper Denkers <j.denkers@tudelft.nl>
2019-10-29 18:39:04 +0100

    Expand recovery tests with multiline inputs

--- org.spoofax.jsglr2.integrationtest/src/test/java/org/spoofax/jsglr2/integrationtest/recovery/RecoveryTest.java
+++ org.spoofax.jsglr2.integrationtest/src/test/java/org/spoofax/jsglr2/integrationtest/recovery/RecoveryTest.java
@@ -15,11 +15,11 @@ public class RecoveryTest extends BaseTestWithRecoverySdf3ParseTables {
         super("recovery.sdf3");
     }
 
-    @Test public void testX() throws ParseError {
+    @Test public void testSingleLineX() throws ParseError {
         testParseSuccess("x");
     }
 
-    @Test public void testSingleY() throws ParseError {
+    @Test public void testSingleLineY() throws ParseError {
         testRecovery("y");
 
         testRecoveryTraced("y", recoveryTrace -> {
@@ -37,7 +37,7 @@ public class RecoveryTest extends BaseTestWithRecoverySdf3ParseTables {
         });
     }
 
-    @Test public void testYY() throws ParseError {
+    @Test public void testSingleLineYY() throws ParseError {
         testRecovery("y y");
 
         testRecoveryTraced("y y", recoveryTrace -> {
@@ -67,7 +67,7 @@ public class RecoveryTest extends BaseTestWithRecoverySdf3ParseTables {
         });
     }
 
-    @Test public void testYXY() throws ParseError {
+    @Test public void testSingleLineYXY() throws ParseError {
         testRecovery("yxy");
 
         testRecoveryTraced("yxy", recoveryTrace -> {
@@ -94,6 +94,35 @@ public class RecoveryTest extends BaseTestWithRecoverySdf3ParseTables {
                 new RecoverIteration(1, 3, 0)
             //@formatter:on
             ), recoveryTrace.iterations);
+        });
+    }
+
+    @Test public void testMultiLineX() throws ParseError {
+        testParseSuccess("x" + newlines(1) + "x");
+        testParseSuccess("x" + newlines(10) + "x");
+    }
+
+    @Test public void testMultiLineY() throws ParseError {
+        testRecovery("" + newlines(1) + "y");
+        testRecovery("" + newlines(3) + "y");
+
+        testRecoveryTraced(newlines(1) + "y ", recoveryTrace -> {
+            assertEquals(Arrays.asList(0, 1), recoveryTrace.backtrackChoicePoints);
+            assertEquals(Arrays.asList(2), recoveryTrace.started);
+            assertEquals(Arrays.asList(3), recoveryTrace.ended);
+            assertEquals(Arrays.asList(new RecoverIteration(0, 2, 1)), recoveryTrace.iterations);
+        });
+        testRecoveryTraced(newlines(1) + " y ", recoveryTrace -> {
+            assertEquals(Arrays.asList(0, 1), recoveryTrace.backtrackChoicePoints);
+            assertEquals(Arrays.asList(3), recoveryTrace.started);
+            assertEquals(Arrays.asList(4), recoveryTrace.ended);
+            assertEquals(Arrays.asList(new RecoverIteration(0, 3, 1)), recoveryTrace.iterations);
+        });
+        testRecoveryTraced(newlines(3) + "y ", recoveryTrace -> {
+            assertEquals(Arrays.asList(0, 1, 2, 3), recoveryTrace.backtrackChoicePoints);
+            assertEquals(Arrays.asList(4), recoveryTrace.started);
+            assertEquals(Arrays.asList(5), recoveryTrace.ended);
+            assertEquals(Arrays.asList(new RecoverIteration(0, 4, 3)), recoveryTrace.iterations);
         });
     }
 

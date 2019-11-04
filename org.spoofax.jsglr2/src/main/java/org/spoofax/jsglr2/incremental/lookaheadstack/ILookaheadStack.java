87d38b0a8f29c67aa5dcaa47040845eb00a1b134
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2019-11-04 13:49:17 +0100

    Add TODO for recovery optimization

--- org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/incremental/lookaheadstack/ILookaheadStack.java
+++ org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/incremental/lookaheadstack/ILookaheadStack.java
@@ -10,4 +10,9 @@ public interface ILookaheadStack extends IActionQuery, Cloneable {
     void popLookahead();
 
     IncrementalParseForest get();
+
+    // TODO add support for recovery. Resetting should push the characters between new and old position on the stack.
+    // This is an optimization because the parse nodes in the the part of the input being recovered need to be broken
+    // down anyway.
+    // void resetPosition(int position);
 }

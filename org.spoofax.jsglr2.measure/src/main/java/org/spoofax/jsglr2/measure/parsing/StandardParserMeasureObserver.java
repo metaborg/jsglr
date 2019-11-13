6b0fd96e4a28c2f2bffbc31559aa9a864c17c4c0
Jasper Denkers <j.denkers@tudelft.nl>
2019-11-13 20:56:15 +0100

    Fix stack node types mismatch

--- org.spoofax.jsglr2.measure/src/main/java/org/spoofax/jsglr2/measure/parsing/StandardParserMeasureObserver.java
+++ org.spoofax.jsglr2.measure/src/main/java/org/spoofax/jsglr2/measure/parsing/StandardParserMeasureObserver.java
@@ -5,7 +5,7 @@ import org.spoofax.jsglr2.parseforest.IParseForest;
 import org.spoofax.jsglr2.parseforest.IParseNode;
 import org.spoofax.jsglr2.parser.AbstractParseState;
 import org.spoofax.jsglr2.stack.StackLink;
-import org.spoofax.jsglr2.stack.basic.BasicStackNode;
+import org.spoofax.jsglr2.stack.hybrid.HybridStackNode;
 
 class StandardParserMeasureObserver
 //@formatter:off
@@ -14,9 +14,9 @@ class StandardParserMeasureObserver
     ParseNode   extends IParseNode<ParseForest, Derivation>>
 //@formatter:on
     extends
-    ParserMeasureObserver<ParseForest, Derivation, ParseNode, BasicStackNode<ParseForest>, AbstractParseState<?, BasicStackNode<ParseForest>>> {
+    ParserMeasureObserver<ParseForest, Derivation, ParseNode, HybridStackNode<ParseForest>, AbstractParseState<?, HybridStackNode<ParseForest>>> {
 
-    @Override int stackNodeLinkCount(BasicStackNode<ParseForest> stackNode) {
+    @Override int stackNodeLinkCount(HybridStackNode<ParseForest> stackNode) {
         int linksOutCount = 0;
 
         for(StackLink<?, ?> link : stackNode.getLinks())

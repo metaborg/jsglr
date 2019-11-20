5178ea3fe7f26bfb5b2dab43096a762a3d55af03
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2019-11-20 10:38:43 +0100

    Slightly rearrange comment for saving choice points each line

--- org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/recovery/AbstractRecoveryParseState.java
+++ org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/recovery/AbstractRecoveryParseState.java
@@ -29,10 +29,10 @@ public abstract class AbstractRecoveryParseState
 
     @Override public void nextParseRound(ParserObserving observing) {
         super.nextParseRound(observing);
-
-        // Record backtrack choice points per line. If in recovery mode, only record new choice points when parsing
-        // after the point that initiated recovery.
         int currentOffset = inputStack.offset();
+
+        // Record backtrack choice points per line.
+        // If in recovery mode, only record new choice points when parsing after the point that initiated recovery.
         if((currentOffset == 0 || CharacterClassFactory.isNewLine(inputStack.getChar(currentOffset - 1)))
             && (!isRecovering() || lastBacktrackChoicePoint().offset() < currentOffset)) {
             IBacktrackChoicePoint<?, StackNode> choicePoint = saveBacktrackChoicePoint();

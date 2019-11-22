91ac18145d66d0d604d66392eeee2b6881085028
Gabriel Konat <gabrielkonat@gmail.com>
2019-11-22 16:02:10 +0100

    Update gradle.config version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("org.metaborg.gradle.config.root-project") version "0.3.8"
+  id("org.metaborg.gradle.config.root-project") version "0.3.10"
   id("org.metaborg.gitonium") version "0.1.2"
 }
 
@@ -11,7 +11,7 @@ subprojects {
 
 allprojects {
   version = "2.6.0-SNAPSHOT" // Override version from Git, as Spoofax Core uses a different versioning scheme.
-  
+
   repositories {
     maven("https://pluto-build.github.io/mvnrepository/")
     maven("https://sugar-lang.github.io/mvnrepository/")

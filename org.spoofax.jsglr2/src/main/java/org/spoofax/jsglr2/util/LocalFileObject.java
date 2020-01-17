e238d24fade3237473a2136fddbaac4d9e1de223
Maarten Sijm <M.P.Sijm@student.tudelft.nl>
2020-01-17 14:38:00 +0100

    Use import instead of fully qualified name for FileObject

--- org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/util/LocalFileObject.java
+++ org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/util/LocalFileObject.java
@@ -2,17 +2,18 @@ package org.spoofax.jsglr2.util;
 
 import java.io.File;
 
+import org.apache.commons.vfs2.FileObject;
 import org.apache.commons.vfs2.FileSystemException;
 import org.apache.commons.vfs2.impl.StandardFileSystemManager;
 
 public class LocalFileObject {
 
-    public static org.apache.commons.vfs2.FileObject get(String path) {
+    public static FileObject get(String path) {
         StandardFileSystemManager fileSystemManager = new StandardFileSystemManager();
 
         try {
             fileSystemManager.init();
-            org.apache.commons.vfs2.FileObject resource = fileSystemManager.resolveFile(new File("."), path);
+            FileObject resource = fileSystemManager.resolveFile(new File("."), path);
             fileSystemManager.close();
 
             return resource;

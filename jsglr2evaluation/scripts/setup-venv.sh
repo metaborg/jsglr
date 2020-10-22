b85754c142cab2b5dc014ce087e75afcb83d0a73
Maarten Sijm <mpsijm@gmail.com>
2020-10-22 10:17:43 +0200

    Clean up setup-venv.sh

--- jsglr2evaluation/scripts/setup-venv.sh
+++ jsglr2evaluation/scripts/setup-venv.sh
@@ -8,11 +8,6 @@ VENV_DIR="$PWD/.venv"
 if [[ "${1-}" == "clean" || ! -d "$VENV_DIR" ]]
 then
     rm -rf "$VENV_DIR"
-    # Use --without-pip and manually install Pip via curl, because else it's broken on Ubuntu/Debian:
-    # https://bugs.launchpad.net/ubuntu/+source/python3.4/+bug/1290847/+index?comments=all (comment #58)
-    python3 -m venv --without-pip "$VENV_DIR"
-    source "$VENV_DIR/bin/activate"
-    curl https://bootstrap.pypa.io/get-pip.py | python
-    pip install matplotlib pdftools
-    deactivate
+    python3 -m venv "$VENV_DIR"
+    "$VENV_DIR/bin/python3" -m pip install matplotlib pdftools
 fi

metaborg/jsglr

commit 63cbc311166225196d3563fb9b07945f15840674

Prevent index out of bound on error at end of file

org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/parser/failure/IParseFailureHandler.java
@@ -17,7 +17,8 @@ public interface IParseFailureHandler
     boolean onFailure(ParseState parseState);
 
     default ParseFailureCause failureCause(ParseState parseState) {
-        Position position = Position.atOffset(parseState.inputStack.inputString(), parseState.inputStack.offset());
+        Position position = Position.atOffset(parseState.inputStack.inputString(),
+            Math.min(parseState.inputStack.offset(), parseState.inputStack.length() - 1));
 
         if(parseState.inputStack.offset() < parseState.inputStack.length())
             return new ParseFailureCause(ParseFailureCause.Type.UnexpectedInput, position);

commit 667fe98cd7d6fab82985a3e35a9d82fdad8d9579

Prevent index out of bound on error at begin of file, introduced in 63cbc311

org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/parser/failure/IParseFailureHandler.java
@@ -18,7 +18,7 @@ public interface IParseFailureHandler
 
     default ParseFailureCause failureCause(ParseState parseState) {
         Position position = Position.atOffset(parseState.inputStack.inputString(),
-            Math.min(parseState.inputStack.offset(), parseState.inputStack.length() - 1));
+            Math.max(Math.min(parseState.inputStack.offset(), parseState.inputStack.length() - 1), 0));
 
         if(parseState.inputStack.offset() < parseState.inputStack.length())
             return new ParseFailureCause(ParseFailureCause.Type.UnexpectedInput, position);
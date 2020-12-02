a63474f144c9f9df3589028249206a7cf7cb6adb
Jasper Denkers <j.denkers@tudelft.nl>
2020-12-02 22:40:25 +0100

    Refactor recover messages post processing

--- org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/parser/AbstractParseState.java
+++ org.spoofax.jsglr2/src/main/java/org/spoofax/jsglr2/parser/AbstractParseState.java
@@ -1,11 +1,14 @@
 package org.spoofax.jsglr2.parser;
 
 import java.util.ArrayDeque;
+import java.util.Collection;
 import java.util.Queue;
 
 import org.metaborg.parsetable.query.ParsingMode;
+import org.spoofax.jsglr.client.imploder.ITokens;
 import org.spoofax.jsglr2.JSGLR2Request;
 import org.spoofax.jsglr2.inputstack.IInputStack;
+import org.spoofax.jsglr2.messages.Message;
 import org.spoofax.jsglr2.parser.observing.ParserObserving;
 import org.spoofax.jsglr2.stack.IStackNode;
 import org.spoofax.jsglr2.stack.collections.IActiveStacks;
@@ -37,6 +40,10 @@ public abstract class AbstractParseState<InputStack extends IInputStack, StackNo
 
     public void nextParseRound(ParserObserving observing) {
         observing.notify(observer -> observer.parseRound(this, activeStacks));
+    }
+
+    public Collection<Message> postProcessMessages(Collection<Message> messages, ITokens tokens) {
+        return messages;
     }
 
 }

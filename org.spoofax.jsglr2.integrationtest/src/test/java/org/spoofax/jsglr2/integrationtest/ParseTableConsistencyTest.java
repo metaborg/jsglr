b3acc394547fe006b8c52a7eeb2565864d04933b
Jasper Denkers <j.denkers@tudelft.nl>
2020-12-29 20:48:02 +0100

    Recovery testing not needed for parse table consistency checking

--- org.spoofax.jsglr2.integrationtest/src/test/java/org/spoofax/jsglr2/integrationtest/ParseTableConsistencyTest.java
+++ org.spoofax.jsglr2.integrationtest/src/test/java/org/spoofax/jsglr2/integrationtest/ParseTableConsistencyTest.java
@@ -1,10 +1,10 @@
 package org.spoofax.jsglr2.integrationtest;
 
-public class ParseTableConsistencyTest extends BaseTestWithRecoverySdf3ParseTables {
+public class ParseTableConsistencyTest extends BaseTestWithSdf3ParseTables {
 
     public ParseTableConsistencyTest() {
         // Tests are triggered in BaseTestWithSdf3ParseTables::testParseTableConsistency
-        super("parsetable-consistency.sdf3", true, true);
+        super("parsetable-consistency.sdf3");
     }
 
 }
